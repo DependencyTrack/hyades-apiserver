fe7cb043893e12bcd2b4f926dbcd11d3cb48ddb4
Apurva <apurva.jain@gatech.edu>
2022-12-26 13:51:40 -0600

    Analyzer completion tracker changes

---
+++ src/main/java/org/dependencytrack/event/kafka/processor/ComponentAnalyzerConfigProcessor.java
@@ -0,0 +1,47 @@
+package org.dependencytrack.event.kafka.processor;
+
+import alpine.common.logging.Logger;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.Record;
+import org.dependencytrack.event.kafka.dto.AnalyzerCompletionStatus;
+import org.dependencytrack.event.kafka.dto.AnalyzerConfig;
+import org.dependencytrack.event.kafka.dto.VulnerabilityResult;
+import org.dependencytrack.model.Component;
+import org.dependencytrack.model.Vulnerability;
+import org.dependencytrack.model.VulnerabilityAnalysisLevel;
+import org.dependencytrack.persistence.QueryManager;
+import org.dependencytrack.util.AnalyzerCompletionTracker;
+import org.dependencytrack.util.NotificationUtil;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.UUID;
+
+public class ComponentAnalyzerConfigProcessor implements Processor<UUID, AnalyzerConfig, Void, Void> {
+
+    private static final Logger LOGGER = Logger.getLogger(VulnerabilityResultProcessor.class);
+
+    @Override
+    public void process(final Record<UUID, AnalyzerConfig> record) {
+        final UUID componentUuid = record.key();
+        final AnalyzerConfig result = record.value();
+
+        try (final var qm = new QueryManager()) {
+            final Component component = qm.getObjectByUuid(Component.class, componentUuid);
+            if (component == null) {
+                LOGGER.warn("Component " + componentUuid + " does not exist");
+                return;
+            }
+            LOGGER.info("Received Analyzer config ");
+            LOGGER.info(" - Component: " + component);
+            try {
+                AnalyzerCompletionTracker.analyzerConfigMap.put(componentUuid, result);
+            } finally {
+                // Ensure that the last vulnerability analysis timestamp is always updated.
+                // Alternatively, implement a retry mechanism.
+                qm.runInTransaction(() -> component.setLastVulnerabilityAnalysis(new Date()));
+            }
+        }
+    }
+}
--- src/test/java/org/dependencytrack/event/kafka/KafkaStreamsTopologyTest.java
+++ src/test/java/org/dependencytrack/event/kafka/KafkaStreamsTopologyTest.java
@@ -5,6 +5,7 @@ import org.apache.kafka.streams.TestInputTopic;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.TopologyTestDriver;
 import org.dependencytrack.PersistenceCapableTest;
+import org.dependencytrack.event.kafka.dto.AnalyzerConfig;
 import org.dependencytrack.event.kafka.dto.VulnerabilityResult;
 import org.dependencytrack.event.kafka.serialization.JacksonSerializer;
 import org.dependencytrack.model.Component;
@@ -15,15 +16,22 @@ import org.dependencytrack.model.RepositoryType;
 import org.dependencytrack.model.Vulnerability;
 import org.dependencytrack.tasks.repositories.MetaModel;
 import org.dependencytrack.tasks.scanners.AnalyzerIdentity;
+import org.dependencytrack.util.AnalyzerCompletionTracker;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import javax.inject.Inject;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +41,10 @@ public class KafkaStreamsTopologyTest extends PersistenceCapableTest {
     private TopologyTestDriver testDriver;
     private TestInputTopic<UUID, MetaModel> repoMetaAnalysisResultInputTopic;
     private TestInputTopic<UUID, VulnerabilityResult> vulnAnalysisResultInputTopic;
+    private TestInputTopic<UUID, AnalyzerConfig> analyzerConfigTopic;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
     @Before
     public void setUp() {
@@ -43,17 +55,25 @@ public class KafkaStreamsTopologyTest extends PersistenceCapableTest {
                 new UUIDSerializer(), new JacksonSerializer<>());
         vulnAnalysisResultInputTopic = testDriver.createInputTopic(KafkaTopic.VULN_ANALYSIS_RESULT.getName(),
                 new UUIDSerializer(), new JacksonSerializer<>());
+        analyzerConfigTopic = testDriver.createInputTopic(KafkaTopic.VULN_ANALYSIS_INFO.getName(),
+                new UUIDSerializer(), new JacksonSerializer<>());
+
+        System.setOut(new PrintStream(outContent));
     }
 
     @After
     public void tearDown() {
+        System.setOut(originalOut);
         testDriver.close();
     }
+
 
     @Test
     // FIXME: Currently failing b/c notifications are being dispatched via NotificationUtil, but no Kafka producer
     // is available. Will need to refactor NotificationUtil so that a MockProducer can be injected.
     public void testVulnResultIngestion() {
+
+        AnalyzerConfig analyzerConfig = new AnalyzerConfig(true, true,true);
         var project = new Project();
         project.setName("acme-app");
         project = qm.createProject(project, List.of(), false);
@@ -67,23 +87,67 @@ public class KafkaStreamsTopologyTest extends PersistenceCapableTest {
         reportedVuln.setVulnId("INT-123");
         reportedVuln.setSource(Vulnerability.Source.INTERNAL);
 
+        final var reportedVuln2 = new Vulnerability();
+        reportedVuln2.setVulnId("INT-124");
+        reportedVuln2.setSource(Vulnerability.Source.INTERNAL);
+        List<Vulnerability> vulnerabilities = new ArrayList<>();
+        vulnerabilities.add(reportedVuln);
+        vulnerabilities.add(reportedVuln2);
+
         final Date beforeAnalysis = new Date();
-        vulnAnalysisResultInputTopic.pipeInput(component.getUuid(), new VulnerabilityResult(reportedVuln, AnalyzerIdentity.INTERNAL_ANALYZER));
+        vulnAnalysisResultInputTopic.pipeInput(component.getUuid(), new VulnerabilityResult(vulnerabilities, AnalyzerIdentity.INTERNAL_ANALYZER, false));
 
         qm.getPersistenceManager().refresh(component);
         assertThat(component.getLastVulnerabilityAnalysis()).isAfter(beforeAnalysis);
 
-        final List<Vulnerability> vulnerabilities = qm.getAllVulnerabilities(component);
-        assertThat(vulnerabilities).hasSize(1);
+        final List<Vulnerability> vulns = qm.getAllVulnerabilities(component);
+        assertThat(vulns).hasSize(2);
 
-        final Vulnerability vuln = vulnerabilities.get(0);
+        final Vulnerability vuln = vulns.get(0);
         assertThat(vuln.getVulnId()).isEqualTo("INT-123");
         assertThat(vuln.getSource()).isEqualTo(Vulnerability.Source.INTERNAL.name());
 
         final FindingAttribution attribution = qm.getFindingAttribution(vuln, component);
         assertThat(attribution).isNotNull();
         assertThat(attribution.getAnalyzerIdentity()).isEqualTo(AnalyzerIdentity.INTERNAL_ANALYZER);
+       }
+
+    @Test
+    public void testVulnCompletion(){
+        AnalyzerConfig analyzerConfig = new AnalyzerConfig(true, true,true);
+        var project = new Project();
+        project.setName("acme-app");
+        project = qm.createProject(project, List.of(), false);
+
+        var component = new Component();
+        component.setProject(project);
+        component.setName("acme-lib");
+        component = qm.createComponent(component, false);
+
+        final var reportedVuln = new Vulnerability();
+        reportedVuln.setVulnId("INT-123");
+        reportedVuln.setSource(Vulnerability.Source.INTERNAL);
+
+        final var reportedVuln2 = new Vulnerability();
+        reportedVuln2.setVulnId("INT-124");
+        reportedVuln2.setSource(Vulnerability.Source.INTERNAL);
+        List<Vulnerability> vulnerabilities = new ArrayList<>();
+        vulnerabilities.add(reportedVuln);
+        vulnerabilities.add(reportedVuln2);
+
+        analyzerConfigTopic.pipeInput(component.getUuid(), analyzerConfig);
+        vulnAnalysisResultInputTopic.pipeInput(component.getUuid(), new VulnerabilityResult(vulnerabilities, AnalyzerIdentity.INTERNAL_ANALYZER, false));
+        vulnAnalysisResultInputTopic.pipeInput(component.getUuid(), new VulnerabilityResult(vulnerabilities, AnalyzerIdentity.OSSINDEX_ANALYZER, false));
+
+        assertThat(AnalyzerCompletionTracker.completionTrackMap.get(component.getUuid()).isInternalCompleted()).isEqualTo(true);
+        assertThat(AnalyzerCompletionTracker.completionTrackMap.get(component.getUuid()).isOSSCompleted()).isEqualTo(true);
+        Assert.assertFalse(outContent.toString().contains("completed"));
+
+        vulnAnalysisResultInputTopic.pipeInput(component.getUuid(), new VulnerabilityResult(vulnerabilities, AnalyzerIdentity.SNYK_ANALYZER, false));
+        assertThat(AnalyzerCompletionTracker.completionTrackMap.get(component.getUuid()).isSnykCompleted()).isEqualTo(true);
+        Assert.assertTrue(outContent.toString().contains("completed"));
     }
+
 
     @Test
     public void testVulnResultIngestionNoFinding() {
@@ -97,7 +161,7 @@ public class KafkaStreamsTopologyTest extends PersistenceCapableTest {
         component = qm.createComponent(component, false);
 
         final Date beforeAnalysis = new Date();
-        vulnAnalysisResultInputTopic.pipeInput(component.getUuid(), new VulnerabilityResult(null, AnalyzerIdentity.INTERNAL_ANALYZER));
+        vulnAnalysisResultInputTopic.pipeInput(component.getUuid(), new VulnerabilityResult(null, AnalyzerIdentity.INTERNAL_ANALYZER, false));
 
         qm.getPersistenceManager().refresh(component);
         assertThat(component.getLastVulnerabilityAnalysis()).isAfter(beforeAnalysis);
