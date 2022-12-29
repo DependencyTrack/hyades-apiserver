982fbbf465abf2bb56fb05b5c9abfbb2cd4029ae
Apurva <apurva.jain@gatech.edu>
2022-12-29 16:20:48 -0600

    Bom processing completion changes

--- src/main/java/org/dependencytrack/util/AnalyzerCompletionTracker.java
+++ src/main/java/org/dependencytrack/util/AnalyzerCompletionTracker.java
@@ -3,6 +3,7 @@ package org.dependencytrack.util;
 import org.dependencytrack.event.kafka.dto.AnalyzerCompletionStatus;
 import org.dependencytrack.event.kafka.dto.AnalyzerConfig;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,4 +12,5 @@ public class AnalyzerCompletionTracker {
     public static ConcurrentHashMap<UUID, AnalyzerConfig> analyzerConfigMap = new ConcurrentHashMap<>();
     public static ConcurrentHashMap<UUID, AnalyzerCompletionStatus> completionTrackMap = new ConcurrentHashMap<>();
 
+    public static ConcurrentHashMap<Long, List<UUID>> projectComponentsMap = new ConcurrentHashMap<Long, java.util.List<UUID>>();
 }
