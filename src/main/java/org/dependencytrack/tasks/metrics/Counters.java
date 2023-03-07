b966af87560cd1abea888f72d7e5dfe017ebe208
mehab <meha.bhargava2@gmail.com>
2023-03-07 14:47:57 +0000

    removed unused getters and setters
    Signed-off-by: mehab <meha.bhargava2@gmail.com>

--- src/main/java/org/dependencytrack/tasks/metrics/Counters.java
+++ src/main/java/org/dependencytrack/tasks/metrics/Counters.java
@@ -20,9 +20,6 @@ package org.dependencytrack.tasks.metrics;
 
 import org.dependencytrack.model.Component;
 import org.dependencytrack.model.DependencyMetrics;
-import org.dependencytrack.model.PortfolioMetrics;
-import org.dependencytrack.model.Project;
-import org.dependencytrack.model.ProjectMetrics;
 
 import java.util.Date;
 
@@ -37,7 +34,7 @@ public final class Counters {
 
     int critical, high, medium, low, unassigned;
     double inheritedRiskScore;
-    int components, vulnerableComponents, projects, vulnerableProjects;
+    int components, projects;
     int vulnerabilities, suppressions, findingsTotal, findingsAudited, findingsUnaudited;
     int policyViolationsFail, policyViolationsWarn, policyViolationsInfo,
             policyViolationsTotal, policyViolationsAudited, policyViolationsUnaudited,
@@ -89,24 +86,12 @@ public final class Counters {
         return inheritedRiskScore;
     }
 
-    public void setInheritedRiskScore(double inheritedRiskScore) {
-        this.inheritedRiskScore = inheritedRiskScore;
-    }
-
     public int getComponents() {
         return components;
     }
 
     public void setComponents(int components) {
         this.components = components;
-    }
-
-    public int getVulnerableComponents() {
-        return vulnerableComponents;
-    }
-
-    public void setVulnerableComponents(int vulnerableComponents) {
-        this.vulnerableComponents = vulnerableComponents;
     }
 
     public int getProjects() {
@@ -117,176 +102,12 @@ public final class Counters {
         this.projects = projects;
     }
 
-    public int getVulnerableProjects() {
-        return vulnerableProjects;
-    }
-
-    public void setVulnerableProjects(int vulnerableProjects) {
-        this.vulnerableProjects = vulnerableProjects;
-    }
-
     public int getVulnerabilities() {
         return vulnerabilities;
     }
 
     public void setVulnerabilities(int vulnerabilities) {
         this.vulnerabilities = vulnerabilities;
-    }
-
-    public int getSuppressions() {
-        return suppressions;
-    }
-
-    public void setSuppressions(int suppressions) {
-        this.suppressions = suppressions;
-    }
-
-    public int getFindingsTotal() {
-        return findingsTotal;
-    }
-
-    public void setFindingsTotal(int findingsTotal) {
-        this.findingsTotal = findingsTotal;
-    }
-
-    public int getFindingsAudited() {
-        return findingsAudited;
-    }
-
-    public void setFindingsAudited(int findingsAudited) {
-        this.findingsAudited = findingsAudited;
-    }
-
-    public int getFindingsUnaudited() {
-        return findingsUnaudited;
-    }
-
-    public void setFindingsUnaudited(int findingsUnaudited) {
-        this.findingsUnaudited = findingsUnaudited;
-    }
-
-    public int getPolicyViolationsFail() {
-        return policyViolationsFail;
-    }
-
-    public void setPolicyViolationsFail(int policyViolationsFail) {
-        this.policyViolationsFail = policyViolationsFail;
-    }
-
-    public int getPolicyViolationsWarn() {
-        return policyViolationsWarn;
-    }
-
-    public void setPolicyViolationsWarn(int policyViolationsWarn) {
-        this.policyViolationsWarn = policyViolationsWarn;
-    }
-
-    public int getPolicyViolationsInfo() {
-        return policyViolationsInfo;
-    }
-
-    public void setPolicyViolationsInfo(int policyViolationsInfo) {
-        this.policyViolationsInfo = policyViolationsInfo;
-    }
-
-    public int getPolicyViolationsTotal() {
-        return policyViolationsTotal;
-    }
-
-    public void setPolicyViolationsTotal(int policyViolationsTotal) {
-        this.policyViolationsTotal = policyViolationsTotal;
-    }
-
-    public int getPolicyViolationsAudited() {
-        return policyViolationsAudited;
-    }
-
-    public void setPolicyViolationsAudited(int policyViolationsAudited) {
-        this.policyViolationsAudited = policyViolationsAudited;
-    }
-
-    public int getPolicyViolationsUnaudited() {
-        return policyViolationsUnaudited;
-    }
-
-    public void setPolicyViolationsUnaudited(int policyViolationsUnaudited) {
-        this.policyViolationsUnaudited = policyViolationsUnaudited;
-    }
-
-    public int getPolicyViolationsSecurityTotal() {
-        return policyViolationsSecurityTotal;
-    }
-
-    public void setPolicyViolationsSecurityTotal(int policyViolationsSecurityTotal) {
-        this.policyViolationsSecurityTotal = policyViolationsSecurityTotal;
-    }
-
-    public int getPolicyViolationsSecurityAudited() {
-        return policyViolationsSecurityAudited;
-    }
-
-    public void setPolicyViolationsSecurityAudited(int policyViolationsSecurityAudited) {
-        this.policyViolationsSecurityAudited = policyViolationsSecurityAudited;
-    }
-
-    public int getPolicyViolationsSecurityUnaudited() {
-        return policyViolationsSecurityUnaudited;
-    }
-
-    public void setPolicyViolationsSecurityUnaudited(int policyViolationsSecurityUnaudited) {
-        this.policyViolationsSecurityUnaudited = policyViolationsSecurityUnaudited;
-    }
-
-    public int getPolicyViolationsLicenseTotal() {
-        return policyViolationsLicenseTotal;
-    }
-
-    public void setPolicyViolationsLicenseTotal(int policyViolationsLicenseTotal) {
-        this.policyViolationsLicenseTotal = policyViolationsLicenseTotal;
-    }
-
-    public int getPolicyViolationsLicenseAudited() {
-        return policyViolationsLicenseAudited;
-    }
-
-    public void setPolicyViolationsLicenseAudited(int policyViolationsLicenseAudited) {
-        this.policyViolationsLicenseAudited = policyViolationsLicenseAudited;
-    }
-
-    public int getPolicyViolationsLicenseUnaudited() {
-        return policyViolationsLicenseUnaudited;
-    }
-
-    public void setPolicyViolationsLicenseUnaudited(int policyViolationsLicenseUnaudited) {
-        this.policyViolationsLicenseUnaudited = policyViolationsLicenseUnaudited;
-    }
-
-    public int getPolicyViolationsOperationalTotal() {
-        return policyViolationsOperationalTotal;
-    }
-
-    public void setPolicyViolationsOperationalTotal(int policyViolationsOperationalTotal) {
-        this.policyViolationsOperationalTotal = policyViolationsOperationalTotal;
-    }
-
-    public int getPolicyViolationsOperationalAudited() {
-        return policyViolationsOperationalAudited;
-    }
-
-    public void setPolicyViolationsOperationalAudited(int policyViolationsOperationalAudited) {
-        this.policyViolationsOperationalAudited = policyViolationsOperationalAudited;
-    }
-
-    public int getPolicyViolationsOperationalUnaudited() {
-        return policyViolationsOperationalUnaudited;
-    }
-
-    public void setPolicyViolationsOperationalUnaudited(int policyViolationsOperationalUnaudited) {
-        this.policyViolationsOperationalUnaudited = policyViolationsOperationalUnaudited;
-    }
-
-    public Date getMeasuredAt() {
-        return measuredAt;
     }
 
     final Date measuredAt;
@@ -309,79 +130,6 @@ public final class Counters {
         metrics.setFindingsTotal(this.findingsTotal);
         metrics.setFindingsAudited(this.findingsAudited);
         metrics.setFindingsUnaudited(this.findingsUnaudited);
-        metrics.setInheritedRiskScore(this.inheritedRiskScore);
-        metrics.setPolicyViolationsFail(this.policyViolationsFail);
-        metrics.setPolicyViolationsWarn(this.policyViolationsWarn);
-        metrics.setPolicyViolationsInfo(this.policyViolationsInfo);
-        metrics.setPolicyViolationsTotal(this.policyViolationsTotal);
-        metrics.setPolicyViolationsAudited(this.policyViolationsAudited);
-        metrics.setPolicyViolationsUnaudited(this.policyViolationsUnaudited);
-        metrics.setPolicyViolationsSecurityTotal(this.policyViolationsSecurityTotal);
-        metrics.setPolicyViolationsSecurityAudited(this.policyViolationsSecurityAudited);
-        metrics.setPolicyViolationsSecurityUnaudited(this.policyViolationsSecurityUnaudited);
-        metrics.setPolicyViolationsLicenseTotal(this.policyViolationsLicenseTotal);
-        metrics.setPolicyViolationsLicenseAudited(this.policyViolationsLicenseAudited);
-        metrics.setPolicyViolationsLicenseUnaudited(this.policyViolationsLicenseUnaudited);
-        metrics.setPolicyViolationsOperationalTotal(this.policyViolationsOperationalTotal);
-        metrics.setPolicyViolationsOperationalAudited(this.policyViolationsOperationalAudited);
-        metrics.setPolicyViolationsOperationalUnaudited(this.policyViolationsOperationalUnaudited);
-        metrics.setFirstOccurrence(this.measuredAt);
-        metrics.setLastOccurrence(this.measuredAt);
-        return metrics;
-    }
-
-    public ProjectMetrics createProjectMetrics(final Project project) {
-        final var metrics = new ProjectMetrics();
-        metrics.setProject(project);
-        metrics.setCritical(this.critical);
-        metrics.setHigh(this.high);
-        metrics.setMedium(this.medium);
-        metrics.setLow(this.low);
-        metrics.setUnassigned(this.unassigned);
-        metrics.setVulnerabilities(this.vulnerabilities);
-        metrics.setComponents(this.components);
-        metrics.setVulnerableComponents(this.vulnerableComponents);
-        metrics.setSuppressed(this.suppressions);
-        metrics.setFindingsTotal(this.findingsTotal);
-        metrics.setFindingsAudited(this.findingsAudited);
-        metrics.setFindingsUnaudited(this.findingsUnaudited);
-        metrics.setInheritedRiskScore(this.inheritedRiskScore);
-        metrics.setPolicyViolationsFail(this.policyViolationsFail);
-        metrics.setPolicyViolationsWarn(this.policyViolationsWarn);
-        metrics.setPolicyViolationsInfo(this.policyViolationsInfo);
-        metrics.setPolicyViolationsTotal(this.policyViolationsTotal);
-        metrics.setPolicyViolationsAudited(this.policyViolationsAudited);
-        metrics.setPolicyViolationsUnaudited(this.policyViolationsUnaudited);
-        metrics.setPolicyViolationsSecurityTotal(this.policyViolationsSecurityTotal);
-        metrics.setPolicyViolationsSecurityAudited(this.policyViolationsSecurityAudited);
-        metrics.setPolicyViolationsSecurityUnaudited(this.policyViolationsSecurityUnaudited);
-        metrics.setPolicyViolationsLicenseTotal(this.policyViolationsLicenseTotal);
-        metrics.setPolicyViolationsLicenseAudited(this.policyViolationsLicenseAudited);
-        metrics.setPolicyViolationsLicenseUnaudited(this.policyViolationsLicenseUnaudited);
-        metrics.setPolicyViolationsOperationalTotal(this.policyViolationsOperationalTotal);
-        metrics.setPolicyViolationsOperationalAudited(this.policyViolationsOperationalAudited);
-        metrics.setPolicyViolationsOperationalUnaudited(this.policyViolationsOperationalUnaudited);
-        metrics.setFirstOccurrence(this.measuredAt);
-        metrics.setLastOccurrence(this.measuredAt);
-        return metrics;
-    }
-
-    public PortfolioMetrics createPortfolioMetrics() {
-        final var metrics = new PortfolioMetrics();
-        metrics.setCritical(this.critical);
-        metrics.setHigh(this.high);
-        metrics.setMedium(this.medium);
-        metrics.setLow(this.low);
-        metrics.setUnassigned(this.unassigned);
-        metrics.setVulnerabilities(this.vulnerabilities);
-        metrics.setComponents(this.components);
-        metrics.setVulnerableComponents(this.vulnerableComponents);
-        metrics.setSuppressed(this.suppressions);
-        metrics.setFindingsTotal(this.findingsTotal);
-        metrics.setFindingsAudited(this.findingsAudited);
-        metrics.setFindingsUnaudited(this.findingsUnaudited);
-        metrics.setProjects(this.projects);
-        metrics.setVulnerableProjects(this.vulnerableProjects);
         metrics.setInheritedRiskScore(this.inheritedRiskScore);
         metrics.setPolicyViolationsFail(this.policyViolationsFail);
         metrics.setPolicyViolationsWarn(this.policyViolationsWarn);
@@ -431,72 +179,6 @@ public final class Counters {
                 || comparedTo.getPolicyViolationsOperationalTotal() != this.policyViolationsOperationalTotal
                 || comparedTo.getPolicyViolationsOperationalAudited() != this.policyViolationsOperationalAudited
                 || comparedTo.getPolicyViolationsOperationalUnaudited() != this.policyViolationsOperationalUnaudited;
-    }
-
-    public boolean hasChanged(final ProjectMetrics comparedTo) {
-        return comparedTo == null
-                || comparedTo.getCritical() != this.critical
-                || comparedTo.getHigh() != this.high
-                || comparedTo.getMedium() != this.medium
-                || comparedTo.getLow() != this.low
-                || comparedTo.getUnassigned() != this.unassigned
-                || comparedTo.getVulnerabilities() != this.vulnerabilities
-                || comparedTo.getSuppressed() != this.suppressions
-                || comparedTo.getFindingsTotal() != this.findingsTotal
-                || comparedTo.getFindingsAudited() != this.findingsAudited
-                || comparedTo.getFindingsUnaudited() != this.findingsUnaudited
-                || comparedTo.getInheritedRiskScore() != this.inheritedRiskScore
-                || comparedTo.getPolicyViolationsFail() != this.policyViolationsFail
-                || comparedTo.getPolicyViolationsWarn() != this.policyViolationsWarn
-                || comparedTo.getPolicyViolationsInfo() != this.policyViolationsInfo
-                || comparedTo.getPolicyViolationsTotal() != this.policyViolationsTotal
-                || comparedTo.getPolicyViolationsAudited() != this.policyViolationsAudited
-                || comparedTo.getPolicyViolationsUnaudited() != this.policyViolationsUnaudited
-                || comparedTo.getPolicyViolationsSecurityTotal() != this.policyViolationsSecurityTotal
-                || comparedTo.getPolicyViolationsSecurityAudited() != this.policyViolationsSecurityAudited
-                || comparedTo.getPolicyViolationsSecurityUnaudited() != this.policyViolationsSecurityUnaudited
-                || comparedTo.getPolicyViolationsLicenseTotal() != this.policyViolationsLicenseTotal
-                || comparedTo.getPolicyViolationsLicenseAudited() != this.policyViolationsLicenseAudited
-                || comparedTo.getPolicyViolationsLicenseUnaudited() != this.policyViolationsLicenseUnaudited
-                || comparedTo.getPolicyViolationsOperationalTotal() != this.policyViolationsOperationalTotal
-                || comparedTo.getPolicyViolationsOperationalAudited() != this.policyViolationsOperationalAudited
-                || comparedTo.getPolicyViolationsOperationalUnaudited() != this.policyViolationsOperationalUnaudited
-                || comparedTo.getComponents() != this.components
-                || comparedTo.getVulnerableComponents() != this.vulnerableComponents;
-    }
-
-    public boolean hasChanged(final PortfolioMetrics comparedTo) {
-        return comparedTo == null
-                || comparedTo.getCritical() != this.critical
-                || comparedTo.getHigh() != this.high
-                || comparedTo.getMedium() != this.medium
-                || comparedTo.getLow() != this.low
-                || comparedTo.getUnassigned() != this.unassigned
-                || comparedTo.getVulnerabilities() != this.vulnerabilities
-                || comparedTo.getInheritedRiskScore() != this.inheritedRiskScore
-                || comparedTo.getPolicyViolationsFail() != this.policyViolationsFail
-                || comparedTo.getPolicyViolationsWarn() != this.policyViolationsWarn
-                || comparedTo.getPolicyViolationsInfo() != this.policyViolationsInfo
-                || comparedTo.getPolicyViolationsTotal() != this.policyViolationsTotal
-                || comparedTo.getPolicyViolationsAudited() != this.policyViolationsAudited
-                || comparedTo.getPolicyViolationsUnaudited() != this.policyViolationsUnaudited
-                || comparedTo.getPolicyViolationsSecurityTotal() != this.policyViolationsSecurityTotal
-                || comparedTo.getPolicyViolationsSecurityAudited() != this.policyViolationsSecurityAudited
-                || comparedTo.getPolicyViolationsSecurityUnaudited() != this.policyViolationsSecurityUnaudited
-                || comparedTo.getPolicyViolationsLicenseTotal() != this.policyViolationsLicenseTotal
-                || comparedTo.getPolicyViolationsLicenseAudited() != this.policyViolationsLicenseAudited
-                || comparedTo.getPolicyViolationsLicenseUnaudited() != this.policyViolationsLicenseUnaudited
-                || comparedTo.getPolicyViolationsOperationalTotal() != this.policyViolationsOperationalTotal
-                || comparedTo.getPolicyViolationsOperationalAudited() != this.policyViolationsOperationalAudited
-                || comparedTo.getPolicyViolationsOperationalUnaudited() != this.policyViolationsOperationalUnaudited
-                || comparedTo.getComponents() != this.components
-                || comparedTo.getVulnerableComponents() != this.vulnerableComponents
-                || comparedTo.getSuppressed() != this.suppressions
-                || comparedTo.getFindingsTotal() != this.findingsTotal
-                || comparedTo.getFindingsAudited() != this.findingsAudited
-                || comparedTo.getFindingsUnaudited() != this.findingsUnaudited
-                || comparedTo.getProjects() != this.projects
-                || comparedTo.getVulnerableProjects() != this.vulnerableProjects;
     }
 
 }
