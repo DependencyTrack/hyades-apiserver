89d62204ea7d5541bf938d017bc679dcb5f9cf0f
nscuro <nscuro@protonmail.com>
2023-04-04 16:09:41 +0200

    Fix KafkaStreamsTopologyTest
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/test/java/org/dependencytrack/event/kafka/KafkaStreamsTopologyTest.java
+++ src/test/java/org/dependencytrack/event/kafka/KafkaStreamsTopologyTest.java
@@ -10,11 +10,9 @@ import org.dependencytrack.model.RepositoryMetaComponent;
 import org.dependencytrack.model.RepositoryType;
 import org.dependencytrack.model.VulnerabilityScan;
 import org.hyades.proto.repometaanalysis.v1.AnalysisResult;
-import org.hyades.proto.vuln.v1.Source;
-import org.hyades.proto.vulnanalysis.v1.Component;
-import org.hyades.proto.vulnanalysis.v1.ScanCommand;
 import org.hyades.proto.vulnanalysis.v1.ScanKey;
 import org.hyades.proto.vulnanalysis.v1.ScanResult;
+import org.hyades.proto.vulnanalysis.v1.ScannerResult;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -27,13 +25,12 @@ import java.util.UUID;
 import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 import static org.dependencytrack.assertion.Assertions.assertConditionWithTimeout;
 import static org.hyades.proto.vuln.v1.Source.SOURCE_OSSINDEX;
 import static org.hyades.proto.vuln.v1.Source.SOURCE_SNYK;
-import static org.hyades.proto.vulnanalysis.v1.ScanStatus.SCAN_STATUS_COMPLETE;
 import static org.hyades.proto.vulnanalysis.v1.ScanStatus.SCAN_STATUS_SUCCESSFUL;
 import static org.hyades.proto.vulnanalysis.v1.Scanner.SCANNER_INTERNAL;
-import static org.hyades.proto.vulnanalysis.v1.Scanner.SCANNER_NONE;
 import static org.hyades.proto.vulnanalysis.v1.Scanner.SCANNER_OSSINDEX;
 import static org.hyades.proto.vulnanalysis.v1.Scanner.SCANNER_SNYK;
 
@@ -114,26 +111,33 @@ public class KafkaStreamsTopologyTest extends KafkaStreamsTest {
                 .setSource(SOURCE_OSSINDEX)
                 .build();
 
-//        kafka.send(SendKeyValues.to(KafkaTopics.VULN_ANALYSIS_RESULT.name(), List.of(
-//                        new KeyValue<>(scanKeyComponentA,
-//                                ScanResult.newBuilder()
-//                                        .setKey(scanKeyComponentA)
-//                                        .setScanner(SCANNER_SNYK)
-//                                        .setStatus(SCAN_STATUS_SUCCESSFUL)
-//                                        .addVulnerabilities(vulnComponentA)
-//                                        .build()),
-//                        new KeyValue<>(scanKeyComponentB,
-//                                ScanResult.newBuilder()
-//                                        .setKey(scanKeyComponentB)
-//                                        .setScanner(SCANNER_OSSINDEX)
-//                                        .setStatus(SCAN_STATUS_SUCCESSFUL)
-//                                        .addVulnerabilities(vulnComponentB)
-//                                        .build())))
-//                .with(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class)
-//                .with(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class));
-//
-//        assertConditionWithTimeout(() -> !qm.getAllVulnerabilities(componentA).isEmpty()
-//                && !qm.getAllVulnerabilities(componentB).isEmpty(), Duration.ofSeconds(5));
+        kafka.send(SendKeyValues.to(KafkaTopics.VULN_ANALYSIS_RESULT.name(), List.of(
+                        new KeyValue<>(scanKeyComponentA,
+                                ScanResult.newBuilder()
+                                        .setKey(scanKeyComponentA)
+                                        .addScannerResults(ScannerResult.newBuilder()
+                                                .setScanner(SCANNER_SNYK)
+                                                .setStatus(SCAN_STATUS_SUCCESSFUL)
+                                                .addVulnerabilities(vulnComponentA))
+                                        .build()),
+                        new KeyValue<>(scanKeyComponentB,
+                                ScanResult.newBuilder()
+                                        .setKey(scanKeyComponentB)
+                                        .addScannerResults(ScannerResult.newBuilder()
+                                                .setScanner(SCANNER_OSSINDEX)
+                                                .setStatus(SCAN_STATUS_SUCCESSFUL)
+                                                .addVulnerabilities(vulnComponentB))
+                                        .build())))
+                .with(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class)
+                .with(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class));
+
+        await()
+                .atMost(Duration.ofSeconds(5))
+                .pollInterval(Duration.ofMillis(250))
+                .untilAsserted(() -> {
+                    assertThat(qm.getAllVulnerabilities(componentA)).hasSize(1);
+                    assertThat(qm.getAllVulnerabilities(componentB)).hasSize(1);
+                });
     }
 
     @Test
@@ -148,46 +152,36 @@ public class KafkaStreamsTopologyTest extends KafkaStreamsTest {
         }
 
         for (final UUID uuid : componentUuids) {
-            kafka.send(SendKeyValues.to(KafkaTopics.VULN_ANALYSIS_COMMAND.name(), List.of(
-                            new KeyValue<>(
-                                    ScanKey.newBuilder()
-                                            .setScanToken(scanToken)
-                                            .setComponentUuid(uuid.toString())
-                                            .build(),
-                                    ScanCommand.newBuilder()
-                                            .setComponent(Component.newBuilder()
-                                                    .setUuid(uuid.toString())
-                                                    .build())
-                                            .build()
-                            ))
-                    )
-                    .with(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class)
-                    .with(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class));
-        }
-
-        for (final UUID uuid : componentUuids) {
             final ScanKey scanKey = ScanKey.newBuilder()
                     .setScanToken(scanToken)
                     .setComponentUuid(uuid.toString())
                     .build();
 
-//            kafka.send(SendKeyValues.to(KafkaTopics.VULN_ANALYSIS_RESULT.name(), List.of(
-//                            new KeyValue<>(
-//                                    scanKey,
-//                                    ScanResult.newBuilder()
-//                                            .setKey(scanKey)
-//                                            .setScanner(SCANNER_NONE)
-//                                            .setStatus(SCAN_STATUS_COMPLETE)
-//                                            .build()))
-//                    )
-//                    .with(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class)
-//                    .with(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class));
+            kafka.send(SendKeyValues.to(KafkaTopics.VULN_ANALYSIS_RESULT.name(), List.of(
+                            new KeyValue<>(
+                                    scanKey,
+                                    ScanResult.newBuilder()
+                                            .setKey(scanKey)
+                                            .addScannerResults(ScannerResult.newBuilder()
+                                                    .setScanner(SCANNER_INTERNAL)
+                                                    .setStatus(SCAN_STATUS_SUCCESSFUL))
+                                            .addScannerResults(ScannerResult.newBuilder()
+                                                    .setScanner(SCANNER_OSSINDEX)
+                                                    .setStatus(SCAN_STATUS_SUCCESSFUL))
+                                            .build()))
+                    )
+                    .with(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class)
+                    .with(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class));
         }
 
-        assertConditionWithTimeout(() -> {
-            qm.getPersistenceManager().refresh(scan);
-            return scan != null && scan.getReceivedResults() == 500;
-        }, Duration.ofSeconds(15));
+        await()
+                .atMost(Duration.ofSeconds(15))
+                .pollInterval(Duration.ofMillis(250))
+                .untilAsserted(() -> {
+                    qm.getPersistenceManager().refresh(scan);
+                    assertThat(scan).isNotNull();
+                    assertThat(scan.getReceivedResults()).isEqualTo(500);
+                });
 
         assertThat(scan.getToken()).isEqualTo(scanToken);
         assertThat(scan.getExpectedResults()).isEqualTo(500);
