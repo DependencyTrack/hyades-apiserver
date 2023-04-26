0e1c36969910e39371e6c9ac9f272dff14f21842
Sahiba Mittal <sahibamittal98@gmail.com>
2023-04-26 16:32:33 +0100

    Bom Upload: don't choke on removed project
    Co-Authored-By: valentijnscholten <4426050+valentijnscholten@users.noreply.github.com>

--- src/main/java/org/dependencytrack/tasks/BomUploadProcessingTask.java
+++ src/main/java/org/dependencytrack/tasks/BomUploadProcessingTask.java
@@ -86,6 +86,10 @@ public class BomUploadProcessingTask implements Subscriber {
             final QueryManager qm = new QueryManager();
             try {
                 final Project project = qm.getObjectByUuid(Project.class, event.getProjectUuid());
+                if (project == null) {
+                    LOGGER.warn("Ignoring BOM Upload event for no longer existing project " + event.getProjectUuid());
+                    return;
+                }
                 bomProcessingFailedProject = project;
                 final List<Component> components;
                 final List<Component> newComponents = new ArrayList<>();
