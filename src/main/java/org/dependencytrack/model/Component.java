7259e02d77c0b27bb00d43e8e37833d9d1875460
Sahiba Mittal <sahiba.mittal@citi.com>
2023-05-31 16:35:17 +0100

    update documentation
    Signed-off-by: Sahiba Mittal <sahiba.mittal@citi.com>

--- src/main/java/org/dependencytrack/model/Component.java
+++ src/main/java/org/dependencytrack/model/Component.java
@@ -244,6 +244,7 @@ public class Component implements Serializable {
     @Persistent(defaultFetchGroup = "true")
     @Index(name = "COMPONENT_PURL_IDX")
     @Column(name = "PURL", jdbcType = "VARCHAR", length = 1024)
+    @Size(max = 1024)
     @com.github.packageurl.validator.PackageURL
     @JsonDeserialize(using = TrimmedStringDeserializer.class)
     private String purl;
