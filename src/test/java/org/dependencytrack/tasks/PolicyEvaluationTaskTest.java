a2793bc063df1368c1efba3bb6bf6ffb006799b2
vithikashukla <vithika.shukla@citi.com>
2023-07-28 12:55:27 +0530

    changed test class name
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>

--- src/test/java/org/dependencytrack/tasks/PolicyEvaluationTaskTest.java
+++ src/test/java/org/dependencytrack/tasks/PolicyEvaluationTaskTest.java
@@ -5,26 +5,38 @@ import org.dependencytrack.AbstractPostgresEnabledTest;
 import org.dependencytrack.event.ComponentPolicyEvaluationEvent;
 import org.dependencytrack.event.ProjectPolicyEvaluationEvent;
 import org.dependencytrack.model.Component;
+import org.dependencytrack.model.License;
+import org.dependencytrack.model.Policy;
+import org.dependencytrack.model.PolicyCondition;
 import org.dependencytrack.model.Project;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.dependencytrack.model.WorkflowStatus.COMPLETED;
 import static org.dependencytrack.model.WorkflowStep.POLICY_EVALUATION;
 
-public class PolicyEvaluatorTaskTest extends AbstractPostgresEnabledTest {
+public class PolicyEvaluationTaskTest extends AbstractPostgresEnabledTest {
 
     @Test
     public void testWorkflowStateIsCompletedForComponent() {
+        License license = new License();
+        license.setName("Apache 2.0");
+        license.setLicenseId("Apache-2.0");
+        license.setUuid(UUID.randomUUID());
+
         var project = new Project();
         project.setName("acme-app");
         project = qm.createProject(project, List.of(), false);
 
         var component = new Component();
+        component.setResolvedLicense(license);
         component.setProject(project);
         component.setName("acme-lib");
         component = qm.createComponent(component, false);
+        Policy policy = qm.createPolicy("Test Policy", Policy.Operator.ANY, Policy.ViolationState.INFO);
+        qm.createPolicyCondition(policy, PolicyCondition.Subject.LICENSE, PolicyCondition.Operator.IS, license.getUuid().toString());
 
         var componentPolicyEvaluationEvent = new ComponentPolicyEvaluationEvent(component.getUuid());
         qm.createWorkflowSteps(componentPolicyEvaluationEvent.getChainIdentifier());
@@ -45,12 +57,7 @@ public class PolicyEvaluatorTaskTest extends AbstractPostgresEnabledTest {
         project.setName("acme-app");
         project = qm.createProject(project, List.of(), false);
 
-        var component = new Component();
-        component.setProject(project);
-        component.setName("acme-lib");
-        component = qm.createComponent(component, false);
-
-        var projectPolicyEvaluationTaskEvent = new ProjectPolicyEvaluationEvent(component.getUuid());
+        var projectPolicyEvaluationTaskEvent = new ProjectPolicyEvaluationEvent(project.getUuid());
         qm.createWorkflowSteps(projectPolicyEvaluationTaskEvent.getChainIdentifier());
         new PolicyEvaluationTask().inform(projectPolicyEvaluationTaskEvent);
 
