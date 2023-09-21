1ecff294c48d56941192697a6caa189c1bf21d59
nscuro <nscuro@protonmail.com>
2023-09-21 14:31:54 +0200

    Add test for vuln severity evaluation
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/test/java/org/dependencytrack/policy/cel/CelPolicyEngineTest.java
+++ src/test/java/org/dependencytrack/policy/cel/CelPolicyEngineTest.java
@@ -121,13 +121,23 @@ public class CelPolicyEngineTest extends AbstractPostgresEnabledTest {
         existingViolation.setTimestamp(new java.util.Date());
         qm.persist(existingViolation);
 
-        final var policyEngine = new CelPolicyEngine();
-        policyEngine.evaluateProject(project.getUuid());
+        new CelPolicyEngine().evaluateProject(project.getUuid());
 
         final List<PolicyViolation> violations = qm.getAllPolicyViolations(component);
         assertThat(violations).isNotEmpty();
     }
 
+    /**
+     * (Regression-)Test for ensuring that all data available in the policy expression context
+     * can be accessed in the expression at runtime.
+     * <p>
+     * Data being available means:
+     * <ul>
+     *   <li>Expression requirements were analyzed correctly</li>
+     *   <li>Database was retrieved from the database correctly</li>
+     *   <li>The mapping from DB data to CEL Protobuf models worked as expected</li>
+     * </ul>
+     */
     @Test
     public void testWithAllFields() {
         final var project = new Project();
@@ -255,7 +265,7 @@ public class CelPolicyEngineTest extends AbstractPostgresEnabledTest {
                   && component.blake2b_512 == "componentBlake2b_512"
                   && component.blake3 == "componentBlake3"
                   && component.license_name == "componentLicenseName"
-                  && !has(component.license_expression)
+                  && !has(component.license_expression) // Requires https://github.com/DependencyTrack/dependency-track/pull/2400 to be ported to Hyades
                   && component.resolved_license.uuid == "__RESOLVED_LICENSE_UUID__"
                   && component.resolved_license.id == "resolvedLicenseId"
                   && component.resolved_license.name == "resolvedLicenseName"
@@ -332,42 +342,6 @@ public class CelPolicyEngineTest extends AbstractPostgresEnabledTest {
     }
 
     @Test
-    public void testIsDirectDependency() {
-        final var policy = qm.createPolicy("policy", Policy.Operator.ANY, Policy.ViolationState.FAIL);
-        final PolicyCondition policyCondition = qm.createPolicyCondition(policy,
-                PolicyCondition.Subject.EXPRESSION, PolicyCondition.Operator.MATCHES, """
-                        component.is_direct_dependency
-                        """);
-        policyCondition.setViolationType(PolicyViolation.Type.OPERATIONAL);
-        qm.persist(policyCondition);
-
-        final var project = new Project();
-        project.setName("foo");
-        qm.persist(project);
-
-        final var componentA = new Component();
-        componentA.setProject(project);
-        componentA.setName("bar");
-        qm.persist(componentA);
-
-        final var componentB = new Component();
-        componentB.setProject(project);
-        componentB.setName("baz");
-        qm.persist(componentB);
-
-        project.setDirectDependencies("[%s]".formatted(new ComponentIdentity(componentA).toJSON()));
-        qm.persist(project);
-        componentA.setDirectDependencies("[%s]".formatted(new ComponentIdentity(componentB).toJSON()));
-        qm.persist(componentA);
-
-        final var policyEngine = new CelPolicyEngine();
-
-        policyEngine.evaluateProject(project.getUuid());
-        assertThat(qm.getAllPolicyViolations(componentA)).hasSize(1);
-        assertThat(qm.getAllPolicyViolations(componentB)).isEmpty();
-    }
-
-    @Test
     public void testProjectDependsOnComponent() {
         final var policy = qm.createPolicy("policy", Policy.Operator.ANY, Policy.ViolationState.FAIL);
         final PolicyCondition policyCondition = qm.createPolicyCondition(policy,
@@ -404,6 +378,55 @@ public class CelPolicyEngineTest extends AbstractPostgresEnabledTest {
     }
 
     @Test
+    public void testVulnerabilitySeverity() {
+        final var policy = qm.createPolicy("policy", Policy.Operator.ANY, Policy.ViolationState.FAIL);
+        qm.createPolicyCondition(policy, PolicyCondition.Subject.EXPRESSION, PolicyCondition.Operator.MATCHES, """
+                vulns.exists(vuln, vuln.severity == "CRITICAL")
+                """, PolicyViolation.Type.SECURITY);
+
+        final var project = new Project();
+        project.setName("acme-app");
+        qm.persist(project);
+
+        final var component = new Component();
+        component.setProject(project);
+        component.setName("acme-lib");
+        qm.persist(component);
+
+        // Create a vulnerability that has all scores (CVSSv2, CVSSv3, OWASP RR)
+        // available, but no severity is set explicitly.
+        //
+        // Even though the expression only accesses the `severity` field, the policy
+        // engine should fetch all scores in order to derive the severity from them.
+        // Note that when multiple scores are available, the highest severity wins.
+        //
+        // The highest severity among the scores below is CRITICAL from CVSSv3.
+
+        final var vuln = new Vulnerability();
+        vuln.setVulnId("CVE-123");
+        vuln.setSource(Vulnerability.Source.NVD);
+        // vuln.setSeverity(Severity.INFO);
+        vuln.setCvssV2BaseScore(BigDecimal.valueOf(6.0));
+        vuln.setCvssV2ImpactSubScore(BigDecimal.valueOf(6.4));
+        vuln.setCvssV2ExploitabilitySubScore(BigDecimal.valueOf(6.8));
+        vuln.setCvssV2Vector("(AV:N/AC:M/Au:S/C:P/I:P/A:P)");
+        vuln.setCvssV3BaseScore(BigDecimal.valueOf(9.1));
+        vuln.setCvssV3ImpactSubScore(BigDecimal.valueOf(5.3));
+        vuln.setCvssV3ExploitabilitySubScore(BigDecimal.valueOf(3.1));
+        vuln.setCvssV3Vector("CVSS:3.0/AV:N/AC:L/PR:L/UI:N/S:C/C:L/I:H/A:L");
+        vuln.setOwaspRRLikelihoodScore(BigDecimal.valueOf(4.5));
+        vuln.setOwaspRRTechnicalImpactScore(BigDecimal.valueOf(5.0));
+        vuln.setOwaspRRBusinessImpactScore(BigDecimal.valueOf(3.75));
+        vuln.setOwaspRRVector("(SL:5/M:5/O:2/S:9/ED:4/EE:2/A:7/ID:2/LC:2/LI:2/LAV:7/LAC:9/FD:3/RD:5/NC:0/PV:7)");
+        qm.persist(vuln);
+
+        qm.addVulnerability(vuln, component, AnalyzerIdentity.INTERNAL_ANALYZER);
+
+        new CelPolicyEngine().evaluateProject(project.getUuid());
+        assertThat(qm.getAllPolicyViolations(component)).hasSize(1);
+    }
+
+    @Test
     public void testMatchesRange() {
         final var policy = qm.createPolicy("policy", Policy.Operator.ANY, Policy.ViolationState.FAIL);
         final PolicyCondition policyCondition = qm.createPolicyCondition(policy,
@@ -431,14 +454,7 @@ public class CelPolicyEngineTest extends AbstractPostgresEnabledTest {
         componentB.setVersion("v2.0.0");
         qm.persist(componentB);
 
-        project.setDirectDependencies("[%s]".formatted(new ComponentIdentity(componentA).toJSON()));
-        qm.persist(project);
-        componentA.setDirectDependencies("[%s]".formatted(new ComponentIdentity(componentB).toJSON()));
-        qm.persist(componentA);
-
-        final var policyEngine = new CelPolicyEngine();
-
-        policyEngine.evaluateProject(project.getUuid());
+        new CelPolicyEngine().evaluateProject(project.getUuid());
         assertThat(qm.getAllPolicyViolations(componentA)).hasSize(1);
         assertThat(qm.getAllPolicyViolations(componentB)).isEmpty();
     }
