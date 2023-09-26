d6eb71b0b62917d914aec3a9049a6d43d2059db4
VithikaS <122881935+VithikaS@users.noreply.github.com>
2023-09-26 10:23:25 +0100

    Add license condition test (#332)
    * Add version cel policy script builder
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    
    * add version support for coordinates cel policy
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    
    * Added unit test for version policy script builder
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    
    * added coordninates condition test
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    
    * added coordinates condition test
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    
    * added more conditions to test
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    
    * Added license condition test
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    
    * Update src/main/java/org/dependencytrack/policy/cel/CelPolicyEngine.java
    
    Co-authored-by: Niklas <nscuro@protonmail.com>
    Signed-off-by: VithikaS <122881935+VithikaS@users.noreply.github.com>
    
    * Added license group condition test
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    
    * updated comment
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    
    ---------
    
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>
    Signed-off-by: VithikaS <122881935+VithikaS@users.noreply.github.com>
    Co-authored-by: vithikashukla <vithika.shukla@citi.com>
    Co-authored-by: Niklas <nscuro@protonmail.com>

--- src/test/java/org/dependencytrack/policy/cel/compat/CoordinatesConditionTest.java
+++ src/test/java/org/dependencytrack/policy/cel/compat/CoordinatesConditionTest.java
@@ -20,20 +20,74 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CoordinatesConditionTest extends AbstractPostgresEnabledTest {
     private Object[] parameters() {
         return new Object[]{
-                // MATCHES regex
-                new Object[]{PolicyCondition.Operator.MATCHES, "{\"group\": \"acme*\",\"name\": \"acme*\",\"version\": \">=v1.2*\"}", "{\"group\": \"acme-app\",\"name\": \"acme-lib\",\"version\": \"v1.2.3\"}", true},
-                //Exact match
-                new Object[]{PolicyCondition.Operator.MATCHES, "{\"group\": \"acme-app\",\"name\": \"acme-lib\",\"version\": \"v1.2.3\"}", "{\"group\": \"acme-app\",\"name\": \"acme-lib\",\"version\": \"v1.2.3\"}", true},
-                //Group does not match
-                new Object[]{PolicyCondition.Operator.MATCHES, "{\"group\": \"org.hippo\",\"name\": \"acme-lib\",\"version\": \"v1.2.3\"}", "{\"group\": \"acme-app\",\"name\": \"acme-lib\",\"version\": \"v1.2.3\"}", false},
+                //MATCHES group regex
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'group': 'acme*','name': 'acme*','version': '>=v1.2*'}", "{'group': 'acme-app','name': 'acme-lib','version': 'v1.2.3'}", true},
+                //Exact matches
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'group': 'acme-app','name': 'acme-lib','version': 'v1.2.3'}", "{'group': 'acme-app','name': 'acme-lib','version': 'v1.2.3'}", true},
+                //Exact group does not match
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'group': 'org.hippo','name': 'acme-lib','version': 'v1.2.3'}", "{'group': 'acme-app','name': 'acme-lib','version': 'v1.2.3'}", false},
                 //Name does not match regex
-                new Object[]{PolicyCondition.Operator.MATCHES, "{\"group\": \"acme-app\",\"name\": \"*acme-lib*\",\"version\": \"v1.2.3\"}", "{\"group\": \"acme-app\",\"name\": \"good-foo-lib\",\"version\": \"v1.2.3\"}", false},
-                //Version does not match
-                new Object[]{PolicyCondition.Operator.MATCHES, "{\"group\": \"acme-app\",\"name\": \"*acme-lib*\",\"version\": \"v1.*\"}", "{\"group\": \"acme-app\",\"name\": \"acme-lib\",\"version\": \"v2.2.3\"}", false},
-                //Does not match on group
-                new Object[]{PolicyCondition.Operator.NO_MATCH, "{\"group\": \"diff-group\",\"name\": \"acme-lib\",\"version\": \"v1.2.3\"}", "{\"group\": \"acme-app\",\"name\": \"acme-lib\",\"version\": \"v1.2.3\"}", true},
-                //Does not match on version
-                new Object[]{PolicyCondition.Operator.NO_MATCH, "{\"group\": \"acme-app\",\"name\": \"*acme-lib*\",\"version\": \">=v2.2.2\"}", "{\"group\": \"acme-app\",\"name\": \"acme-lib\",\"version\": \"v1.2.3\"}", true},
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'group': 'acme-app','name': '*acme-lib*','version': 'v1.2.3'}", "{'group': 'acme-app','name': 'good-foo-lib','version': 'v1.2.3'}", false},
+                //Version regex does not match
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'group': 'acme-app','name': '*acme-lib*','version': 'v1.*'}", "{'group': 'acme-app','name': 'acme-lib','version': 'v2.2.3'}", false},
+                //Does not match on exact group
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{'group': 'diff-group','name': 'acme-lib','version': 'v1.2.3'}", "{'group': 'acme-app','name': 'acme-lib','version': 'v1.2.3'}", true},
+                //Does not match on version range greater than or equal
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{'group': 'acme-app','name': '*acme-lib*','version': '>=v2.2.2'}", "{'group': 'acme-app','name': 'acme-lib','version': 'v1.2.3'}", true},
+                //Matches without group
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'name': 'Test Component','version': '1.0.0'}", "{'name': 'Test Component','version': '1.0.0'}", true},
+                //Matches on wild card - uncomment after fixing script builder
+                //new Object[]{PolicyCondition.Operator.MATCHES, "{ 'group': '*', 'name': 'Test Component', 'version': '1.0.0' }", "{ 'group': 'Anything', 'name': 'Test Component', 'version': '1.0.0' }", false},
+                //Matches on empty policy - uncomment after fixing script builder
+                //new Object[]{PolicyCondition.Operator.MATCHES, "{}", "{}", true},
+                //Does not match on lower version
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{ 'version': '== 1.1.1' }", "{'version': '0.1.1'}", true},
+                //Matches on equal version
+                new Object[]{PolicyCondition.Operator.MATCHES, "{ 'version': '== 1.1.1' }", "{'version': '1.1.1'}", true},
+                //Does not match on higher version
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{ 'version': '== 1.1.1' }", "{'version': '2.1.1'}", true},
+                //No match with version not equal to
+                new Object[]{PolicyCondition.Operator.MATCHES, "{ 'version': '!= 1.1.1' }", "{ 'version': '1.1.1' }", false},
+                //Matches with version not equal to
+                new Object[]{PolicyCondition.Operator.MATCHES, "{ 'version': '!= 1.1.1' }", "{'version': '2.1.1'}", true},
+                //Matches with version not equal to
+                new Object[]{PolicyCondition.Operator.MATCHES, "{ 'version': '!= 1.1.1' }", "{'version': '0.1.1'}", true},
+                //Matches with version greater than
+                new Object[]{PolicyCondition.Operator.MATCHES, "{ 'version': '> 1.1.1' }", "{'version': '2.1.1'}", true},
+                //Does not match on version greater than
+                new Object[]{PolicyCondition.Operator.MATCHES, "{ 'version': '> 1.1.1' }", "{'version': '0.1.1'}", false},
+                //Does not match on version equal to
+                new Object[]{PolicyCondition.Operator.MATCHES, "{ 'version': '> 1.1.1' }", "{'version': '1.1.1'}", false},
+                //No match with version greater than
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{ 'version': '> 1.1.1' }", "{'version': '0.1.1'}", true},
+                //No match with version equal to
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{ 'version': '> 1.1.1' }", "{'version': '1.1.1'}", true},
+                //No match with version greater than
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{ 'version': '> 1.1.1' }", "{'version': '2.1.1'}", false},
+                //Matches on version less than
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'version': '<1.1.1'}", "{'version': '0.1.1'}", true},
+                //Does not match on version less than
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'version': '<1.1.1'}", "{'version': '2.1.1'}", false},
+                //Does not match on equal version
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'version': '<1.1.1'}", "{'version': '1.1.1'}", false},
+                //No match on version less than
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{'version': '<1.1.1'}", "{'version': '0.1.1'}", false},
+                //No match on version less than
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{'version': '<1.1.1'}", "{'version': '2.1.1'}", true},
+                //No match on equal version
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{'version': '<1.1.1'}", "{'version': '1.1.1'}", true},
+                //Matches on version less than equal to
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'version': '<=1.1.1'}", "{'version': '0.1.1'}", true},
+                //Matches on version less than equal to
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'version': '<=1.1.1'}", "{'version': '2.1.1'}", false},
+                //Matches on version less than equal to
+                new Object[]{PolicyCondition.Operator.MATCHES, "{'version': '<=1.1.1'}", "{'version': '1.1.1'}", true},
+                //Matches on version less than equal to
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{'version': '<=1.1.1'}", "{'version': '0.1.1'}", false},
+                //Matches on version less than equal to
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{'version': '<=1.1.1'}", "{'version': '2.1.1'}", true},
+                //Matches on version less than equal to
+                new Object[]{PolicyCondition.Operator.NO_MATCH, "{'version': '<=1.1.1'}", "{'version': '1.1.1'}", false},
         };
     }
 
