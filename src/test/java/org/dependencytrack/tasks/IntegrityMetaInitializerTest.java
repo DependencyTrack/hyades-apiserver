7045063ea113f1aba89866e491956d7176258d47
Sahiba Mittal <sahiba.mittal@citi.com>
2023-09-27 17:32:10 +0100

    Update IntegrityMetaInitializerTest.java

--- src/test/java/org/dependencytrack/tasks/IntegrityMetaInitializerTest.java
+++ src/test/java/org/dependencytrack/tasks/IntegrityMetaInitializerTest.java
@@ -4,21 +4,26 @@ import org.dependencytrack.AbstractPostgresEnabledTest;
 import org.dependencytrack.event.IntegrityMetaInitializer;
 import org.dependencytrack.event.kafka.KafkaTopics;
 import org.dependencytrack.model.Component;
+import org.dependencytrack.model.IntegrityMetaComponent;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.dependencytrack.model.FetchStatus.PROCESSED;
 
 public class IntegrityMetaInitializerTest extends AbstractPostgresEnabledTest {
 
     @Test
-    public void testIntegrityMetaInitializer() {
+    public void testIntegrityMetaInitializerWithNoData() {
 
         IntegrityMetaInitializer initializer = new IntegrityMetaInitializer();
-
         // no existing data in IntegrityMetaComponent
         initializer.contextInitialized(null);
         assertThat(qm.getIntegrityMetaComponentCount()).isEqualTo(0);
         assertThat(kafkaMockProducer.history().size()).isEqualTo(0);
+    }
+
+    @Test
+    public void testIntegrityMetaInitializer() {
 
         final var projectA = qm.createProject("acme-app-a", null, "1.0.0", null, null, null, true, false);
         final var componentProjectA = new Component();
@@ -29,6 +34,7 @@ public class IntegrityMetaInitializerTest extends AbstractPostgresEnabledTest {
         componentProjectA.setPurl("pkg:maven/acme/acme-lib-a@1.0.1?foo=bar");
         qm.persist(componentProjectA);
 
+        IntegrityMetaInitializer initializer = new IntegrityMetaInitializer();
         initializer.contextInitialized(null);
         assertThat(qm.getIntegrityMetaComponentCount()).isEqualTo(1);
         assertThat(qm.getIntegrityMetaComponent(componentProjectA.getPurl().toString())).satisfies(
@@ -47,15 +53,18 @@ public class IntegrityMetaInitializerTest extends AbstractPostgresEnabledTest {
                 record -> assertThat(record.topic()).isEqualTo(KafkaTopics.NOTIFICATION_PROJECT_CREATED.name()),
                 record -> assertThat(record.topic()).isEqualTo(KafkaTopics.REPO_META_ANALYSIS_COMMAND.name())
         );
+    }
 
-        // data now exists in IntegrityMetaComponent so sync will be skipped
-        final var componentProjectB = new Component();
-        componentProjectB.setProject(projectA);
-        componentProjectB.setName("acme-lib-b");
-        componentProjectA.setPurl("pkg:maven/acme/acme-lib-c@3.0.1?foo=bar");
-        qm.persist(componentProjectB);
+    @Test
+    public void testIntegrityMetaInitializerWithExistingData() {
+        var integrityMetaExisting = new IntegrityMetaComponent();
+        integrityMetaExisting.setPurl("pkg:npm/acme/acme-lib-c@3.0.1");
+        integrityMetaExisting.setStatus(PROCESSED);
+        qm.persist(integrityMetaExisting);
+        // data exists in IntegrityMetaComponent so sync will be skipped
+        IntegrityMetaInitializer initializer = new IntegrityMetaInitializer();
         initializer.contextInitialized(null);
-        assertThat(kafkaMockProducer.history().size()).isEqualTo(2);
+        assertThat(kafkaMockProducer.history().size()).isEqualTo(0);
         assertThat(qm.getIntegrityMetaComponentCount()).isEqualTo(1);
     }
 }
