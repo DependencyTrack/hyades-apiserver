DependencyTrack/hyades-apiserver

commit a32f94c3e474f26eff4c8b1f36db9ff497da4d53

added integrity analysis event on apiserver (#339)
* added integrity analysis event on apiserver

Signed-off-by: mehab <meha.bhargava@citi.com>

* fixed unit tests

Signed-off-by: mehab <meha.bhargava@citi.com>

* fixed unit test for repometaanalysistask

Signed-off-by: mehab <meha.bhargava@citi.com>

* fixed tests for bomupload

Signed-off-by: mehab <meha.bhargava@citi.com>

* fixed tests for bomupload

Signed-off-by: mehab <meha.bhargava@citi.com>

* PR review fixes

Signed-off-by: mehab <meha.bhargava@citi.com>


* end to end test successful

Signed-off-by: mehab <meha.bhargava@citi.com>

* fixed broken test

Signed-off-by: mehab <meha.bhargava@citi.com>

---------

Signed-off-by: mehab <meha.bhargava@citi.com>

src/main/java/org/dependencytrack/tasks/BomUploadProcessingTask.java
@@ -40,9 +40,12 @@ import org.dependencytrack.event.ComponentRepositoryMetaAnalysisEvent;
 import org.dependencytrack.event.ComponentVulnerabilityAnalysisEvent;
 import org.dependencytrack.event.ProjectMetricsUpdateEvent;
 import org.dependencytrack.event.kafka.KafkaEventDispatcher;
+import org.dependencytrack.event.kafka.componentmeta.AbstractMetaHandler;
 import org.dependencytrack.model.Bom;
 import org.dependencytrack.model.Component;
 import org.dependencytrack.model.ComponentIdentity;
+import org.dependencytrack.model.FetchStatus;
+import org.dependencytrack.model.IntegrityMetaComponent;
 import org.dependencytrack.model.License;
 import org.dependencytrack.model.Project;
 import org.dependencytrack.model.ServiceComponent;
@@ -58,6 +61,7 @@ import org.dependencytrack.notification.vo.BomConsumedOrProcessed;
 import org.dependencytrack.notification.vo.BomProcessingFailed;
 import org.dependencytrack.persistence.FlushHelper;
 import org.dependencytrack.persistence.QueryManager;
+import org.hyades.proto.repometaanalysis.v1.FetchMeta;
 import org.json.JSONArray;
 
 import javax.jdo.PersistenceManager;
@@ -83,6 +87,8 @@ import java.util.stream.Stream;
 import static org.datanucleus.PropertyNames.PROPERTY_FLUSH_MODE;
 import static org.datanucleus.PropertyNames.PROPERTY_PERSISTENCE_BY_REACHABILITY_AT_COMMIT;
 import static org.dependencytrack.common.ConfigKey.BOM_UPLOAD_PROCESSING_TRX_FLUSH_THRESHOLD;
+import static org.dependencytrack.event.kafka.componentmeta.RepoMetaConstants.SUPPORTED_PACKAGE_URLS_FOR_INTEGRITY_CHECK;
+import static org.dependencytrack.event.kafka.componentmeta.RepoMetaConstants.TIME_SPAN;
 import static org.dependencytrack.parser.cyclonedx.util.ModelConverter.convertComponents;
 import static org.dependencytrack.parser.cyclonedx.util.ModelConverter.convertServices;
 import static org.dependencytrack.parser.cyclonedx.util.ModelConverter.convertToProject;
@@ -315,7 +321,14 @@ public class BomUploadProcessingTask implements Subscriber {
                     // The constructors of ComponentRepositoryMetaAnalysisEvent and ComponentVulnerabilityAnalysisEvent
                     // merely call a few getters on it, but the component object itself is not passed around.
                     // Detaching would imply additional database interactions that we'd rather not do.
-                    repoMetaAnalysisEvents.add(new ComponentRepositoryMetaAnalysisEvent(component));
+                    boolean result = SUPPORTED_PACKAGE_URLS_FOR_INTEGRITY_CHECK.contains(component.getPurl().getType());
+                    ComponentRepositoryMetaAnalysisEvent event;
+                    if (result) {
+                        event = createRepoMetaAnalysisEvent(component, qm);
+                    } else {
+                        event = new ComponentRepositoryMetaAnalysisEvent(component.getPurlCoordinates().toString(), component.isInternal(), FetchMeta.FETCH_META_LATEST_VERSION);
+                    }
+                    repoMetaAnalysisEvents.add(event);
                     vulnAnalysisEvents.add(new ComponentVulnerabilityAnalysisEvent(
                             ctx.uploadToken, component, VulnerabilityAnalysisLevel.BOM_UPLOAD_ANALYSIS, component.isNew()));
                 }
@@ -962,5 +975,21 @@ public class BomUploadProcessingTask implements Subscriber {
         }
 
     }
+
+    private ComponentRepositoryMetaAnalysisEvent createRepoMetaAnalysisEvent(Component component, QueryManager qm) {
+        IntegrityMetaComponent integrityMetaComponent = qm.getIntegrityMetaComponent(component.getPurl().toString());
+        if (integrityMetaComponent == null) {
+            qm.getPersistenceManager().makePersistent(AbstractMetaHandler.createIntegrityMetaComponent(component.getPurl().toString()));
+            return new ComponentRepositoryMetaAnalysisEvent(component.getPurl().canonicalize(), component.isInternal(), FetchMeta.FETCH_META_INTEGRITY_DATA_AND_LATEST_VERSION);
+        }
+        if (integrityMetaComponent.getStatus() == null || (integrityMetaComponent.getStatus() == FetchStatus.IN_PROGRESS && (Date.from(Instant.now()).getTime() - integrityMetaComponent.getLastFetch().getTime()) > TIME_SPAN)) {
+            integrityMetaComponent.setLastFetch(Date.from(Instant.now()));
+            qm.getPersistenceManager().makePersistent(integrityMetaComponent);
+            return new ComponentRepositoryMetaAnalysisEvent(component.getPurl().canonicalize(), component.isInternal(), FetchMeta.FETCH_META_INTEGRITY_DATA_AND_LATEST_VERSION);
+        } else {
+            return new ComponentRepositoryMetaAnalysisEvent(component.getPurlCoordinates().canonicalize(), component.isInternal(), FetchMeta.FETCH_META_LATEST_VERSION);
+        }
+    }
+
 
 }

src/test/java/org/dependencytrack/tasks/BomUploadProcessingTaskTest.java
@@ -18,6 +18,7 @@
  */
 package org.dependencytrack.tasks;
 
+import com.github.packageurl.PackageURL;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.dependencytrack.AbstractPostgresEnabledTest;
 import org.dependencytrack.event.BomUploadEvent;
@@ -27,6 +28,8 @@ import org.dependencytrack.model.Bom;
 import org.dependencytrack.model.Classifier;
 import org.dependencytrack.model.Component;
 import org.dependencytrack.model.ConfigPropertyConstants;
+import org.dependencytrack.model.FetchStatus;
+import org.dependencytrack.model.IntegrityMetaComponent;
 import org.dependencytrack.model.Project;
 import org.dependencytrack.model.VulnerabilityScan;
 import org.dependencytrack.model.WorkflowStep;
@@ -43,6 +46,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -86,6 +90,91 @@ public class BomUploadProcessingTaskTest extends AbstractPostgresEnabledTest {
         final var bomUploadEvent = new BomUploadEvent(qm.detach(Project.class, project.getId()), createTempBomFile("bom-1.xml"));
         qm.createWorkflowSteps(bomUploadEvent.getChainIdentifier());
 
+        new BomUploadProcessingTask().inform(bomUploadEvent);
+        assertConditionWithTimeout(() -> kafkaMockProducer.history().size() >= 5, Duration.ofSeconds(5));
+        assertThat(kafkaMockProducer.history()).satisfiesExactly(
+                event -> assertThat(event.topic()).isEqualTo(KafkaTopics.NOTIFICATION_PROJECT_CREATED.name()),
+                event -> assertThat(event.topic()).isEqualTo(KafkaTopics.NOTIFICATION_BOM.name()),
+                event -> assertThat(event.topic()).isEqualTo(KafkaTopics.VULN_ANALYSIS_COMMAND.name()),
+                event -> assertThat(event.topic()).isEqualTo(KafkaTopics.REPO_META_ANALYSIS_COMMAND.name()),
+                event -> assertThat(event.topic()).isEqualTo(KafkaTopics.NOTIFICATION_BOM.name())
+        );
+        qm.getPersistenceManager().refresh(project);
+        assertThat(project.getClassifier()).isEqualTo(Classifier.APPLICATION);
+        assertThat(project.getLastBomImport()).isNotNull();
+        assertThat(project.getLastBomImportFormat()).isEqualTo("CycloneDX 1.4");
+        assertThat(project.getExternalReferences()).isNotNull();
+        assertThat(project.getExternalReferences()).hasSize(4);
+
+        final List<Component> components = qm.getAllComponents(project);
+        assertThat(components).hasSize(1);
+
+        final Component component = components.get(0);
+        assertThat(component.getAuthor()).isEqualTo("Sometimes this field is long because it is composed of a list of authors......................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................");
+        assertThat(component.getPublisher()).isEqualTo("Example Incorporated");
+        assertThat(component.getGroup()).isEqualTo("com.example");
+        assertThat(component.getName()).isEqualTo("xmlutil");
+        assertThat(component.getVersion()).isEqualTo("1.0.0");
+        assertThat(component.getDescription()).isEqualTo("A makebelieve XML utility library");
+        assertThat(component.getCpe()).isEqualTo("cpe:/a:example:xmlutil:1.0.0");
+        assertThat(component.getPurl().canonicalize()).isEqualTo("pkg:maven/com.example/xmlutil@1.0.0?download_url=https%3A%2F%2Fon-premises.url%2Frepository%2Fnpm%2F%40babel%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration-7.18.6.tgz");
+        assertThat(component.getLicenseUrl()).isEqualTo("https://www.apache.org/licenses/LICENSE-2.0.txt");
+
+        assertThat(qm.getAllWorkflowStatesForAToken(bomUploadEvent.getChainIdentifier())).satisfiesExactlyInAnyOrder(
+                state -> {
+                    assertThat(state.getStep()).isEqualTo(BOM_CONSUMPTION);
+                    assertThat(state.getStatus()).isEqualTo(COMPLETED);
+                    assertThat(state.getStartedAt()).isNotNull();
+                    assertThat(state.getUpdatedAt()).isBefore(Date.from(Instant.now()));
+                },
+                state -> {
+                    assertThat(state.getStep()).isEqualTo(BOM_PROCESSING);
+                    assertThat(state.getStatus()).isEqualTo(COMPLETED);
+                    assertThat(state.getStartedAt()).isNotNull();
+                    assertThat(state.getUpdatedAt()).isBefore(Date.from(Instant.now()));
+                },
+                state -> {
+                    //vuln analysis has not been handled yet, so it will be in pending state
+                    assertThat(state.getStep()).isEqualTo(VULN_ANALYSIS);
+                    assertThat(state.getStatus()).isEqualTo(PENDING);
+                    assertThat(state.getStartedAt()).isBefore(Date.from(Instant.now()));
+                    assertThat(state.getUpdatedAt()).isBefore(Date.from(Instant.now()));
+                },
+                state -> {
+                    //policy evaluation has not been handled yet, so it will be in pending state
+                    assertThat(state.getStep()).isEqualTo(POLICY_EVALUATION);
+                    assertThat(state.getStatus()).isEqualTo(PENDING);
+                    assertThat(state.getParent()).isNotNull();
+                    assertThat(state.getStartedAt()).isNull();
+                    assertThat(state.getUpdatedAt()).isBefore(Date.from(Instant.now()));
+                },
+                state -> {
+                    //metrics update has not been handled yet, so it will be in pending state
+                    assertThat(state.getStep()).isEqualTo(METRICS_UPDATE);
+                    assertThat(state.getStatus()).isEqualTo(PENDING);
+                    assertThat(state.getParent()).isNotNull();
+                    assertThat(state.getStartedAt()).isNull();
+                    assertThat(state.getUpdatedAt()).isBefore(Date.from(Instant.now()));
+                }
+        );
+        final VulnerabilityScan vulnerabilityScan = qm.getVulnerabilityScan(bomUploadEvent.getChainIdentifier().toString());
+        assertThat(vulnerabilityScan).isNotNull();
+        var workflowStatus = qm.getWorkflowStateByTokenAndStep(bomUploadEvent.getChainIdentifier(), WorkflowStep.VULN_ANALYSIS);
+        assertThat(workflowStatus.getStartedAt()).isNotNull();
+    }
+
+    @Test
+    public void informTestWithComponentAlreadyExistsForIntegrityCheck() throws Exception {
+        Project project = qm.createProject("Acme Example", null, "1.0", null, null, null, true, false);
+
+        final var bomUploadEvent = new BomUploadEvent(qm.detach(Project.class, project.getId()), createTempBomFile("bom-1.xml"));
+        qm.createWorkflowSteps(bomUploadEvent.getChainIdentifier());
+        PackageURL packageUrl = new PackageURL("pkg:maven/com.example/xmlutil@1.0.0?download_url=https%3A%2F%2Fon-premises.url%2Frepository%2Fnpm%2F%40babel%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration-7.18.6.tgz");
+        var integrityMeta = new IntegrityMetaComponent();
+        integrityMeta.setPurl("pkg:maven/com.example/xmlutil@1.0.0?download_url=https%3A%2F%2Fon-premises.url%2Frepository%2Fnpm%2F%40babel%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration%2Fhelper-split-export-declaration-7.18.6.tgz");
+        integrityMeta.setStatus(FetchStatus.IN_PROGRESS);
+        integrityMeta.setLastFetch(Date.from(Instant.now().minus(2, ChronoUnit.HOURS)));
+        qm.createIntegrityMetaComponent(integrityMeta);
         new BomUploadProcessingTask().inform(bomUploadEvent);
         assertConditionWithTimeout(() -> kafkaMockProducer.history().size() >= 5, Duration.ofSeconds(5));
         assertThat(kafkaMockProducer.history()).satisfiesExactly(
@@ -433,7 +522,7 @@ public class BomUploadProcessingTaskTest extends AbstractPostgresEnabledTest {
     @Test // https://github.com/DependencyTrack/dependency-track/issues/2519
     public void informIssue2519Test() throws Exception {
         final var project = qm.createProject("Acme Example", null, "1.0", null, null, null, true, false);
-        
+
         // Upload the same BOM again a few times.
         // Ensure processing does not fail, and the number of components ingested doesn't change.
         for (int i = 0; i < 3; i++) {

src/main/java/org/dependencytrack/event/ComponentRepositoryMetaAnalysisEvent.java
@@ -1,21 +1,17 @@
 package org.dependencytrack.event;
 
 import alpine.event.framework.Event;
-import com.github.packageurl.PackageURL;
 import org.dependencytrack.model.Component;
-
-import java.util.Optional;
+import org.hyades.proto.repometaanalysis.v1.FetchMeta;
 
 /**
  * Defines an {@link Event} triggered when requesting a component to be analyzed for meta information.
  *
  * @param purlCoordinates The package URL coordinates of the {@link Component} to analyze
  * @param internal        Whether the {@link Component} is internal
+ * @param fetchMeta       Whether component hash data or component meta data needs to be fetched from external api
  */
-public record ComponentRepositoryMetaAnalysisEvent(String purlCoordinates, Boolean internal) implements Event {
-
-    public ComponentRepositoryMetaAnalysisEvent(final Component component) {
-        this(Optional.ofNullable(component.getPurlCoordinates()).map(PackageURL::canonicalize).orElse(null), component.isInternal());
-    }
+public record ComponentRepositoryMetaAnalysisEvent(String purlCoordinates, Boolean internal,
+                                                   FetchMeta fetchMeta) implements Event {
 
 }

src/main/java/org/dependencytrack/event/IntegrityMetaInitializer.java
@@ -10,6 +10,7 @@ import org.dependencytrack.event.kafka.KafkaEventDispatcher;
 import org.dependencytrack.model.IntegrityMetaComponent;
 import org.dependencytrack.persistence.QueryManager;
 import org.dependencytrack.util.LockProvider;
+import org.hyades.proto.repometaanalysis.v1.FetchMeta;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -67,7 +68,7 @@ public class IntegrityMetaInitializer implements ServletContextListener {
         List<String> purls = qm.fetchNextPurlsPage(offset);
         while (!purls.isEmpty()) {
             long cumulativeProcessingTime = System.currentTimeMillis() - startTime;
-            if(isLockToBeExtended(cumulativeProcessingTime, INTEGRITY_META_INITIALIZER_TASK_LOCK)) {
+            if (isLockToBeExtended(cumulativeProcessingTime, INTEGRITY_META_INITIALIZER_TASK_LOCK)) {
                 LockExtender.extendActiveLock(Duration.ofMinutes(5).plus(lockConfiguration.getLockAtLeastFor()), lockConfiguration.getLockAtLeastFor());
             }
             dispatchPurls(qm, purls);
@@ -88,7 +89,7 @@ public class IntegrityMetaInitializer implements ServletContextListener {
     private void dispatchPurls(QueryManager qm, List<String> purls) {
         for (final var purl : purls) {
             ComponentProjection componentProjection = qm.getComponentByPurl(purl);
-            kafkaEventDispatcher.dispatchAsync(new ComponentRepositoryMetaAnalysisEvent(componentProjection.purlCoordinates, componentProjection.internal));
+            kafkaEventDispatcher.dispatchAsync(new ComponentRepositoryMetaAnalysisEvent(purl, componentProjection.internal, FetchMeta.FETCH_META_INTEGRITY_DATA));
         }
     }
 

src/main/java/org/dependencytrack/event/kafka/KafkaEventConverter.java
@@ -48,7 +48,7 @@ final class KafkaEventConverter {
     }
 
     static KafkaEvent<String, AnalysisCommand> convert(final ComponentRepositoryMetaAnalysisEvent event) {
-        if (event == null || event.purlCoordinates()== null) {
+        if (event == null || event.purlCoordinates() == null) {
             return null;
         }
 
@@ -58,6 +58,7 @@ final class KafkaEventConverter {
 
         final var analysisCommand = AnalysisCommand.newBuilder()
                 .setComponent(componentBuilder)
+                .setFetchMeta(event.fetchMeta())
                 .build();
 
         return new KafkaEvent<>(KafkaTopics.REPO_META_ANALYSIS_COMMAND, event.purlCoordinates(), analysisCommand, null);

src/main/java/org/dependencytrack/event/kafka/componentmeta/AbstractMetaHandler.java
@@ -1,0 +1,28 @@
+package org.dependencytrack.event.kafka.componentmeta;
+
+import org.dependencytrack.event.kafka.KafkaEventDispatcher;
+import org.dependencytrack.model.FetchStatus;
+import org.dependencytrack.model.IntegrityMetaComponent;
+import org.dependencytrack.persistence.QueryManager;
+import org.hyades.proto.repometaanalysis.v1.FetchMeta;
+
+import java.time.Instant;
+import java.util.Date;
+
+public abstract class AbstractMetaHandler implements Handler {
+
+    ComponentProjection componentProjection;
+    QueryManager queryManager;
+    KafkaEventDispatcher kafkaEventDispatcher;
+    FetchMeta fetchMeta;
+
+
+    public static IntegrityMetaComponent createIntegrityMetaComponent(String purl) {
+        IntegrityMetaComponent integrityMetaComponent = new IntegrityMetaComponent();
+        integrityMetaComponent.setStatus(FetchStatus.IN_PROGRESS);
+        integrityMetaComponent.setPurl(purl);
+        integrityMetaComponent.setLastFetch(Date.from(Instant.now()));
+        return integrityMetaComponent;
+    }
+
+}

src/main/java/org/dependencytrack/event/kafka/componentmeta/ComponentProjection.java
@@ -1,0 +1,6 @@
+package org.dependencytrack.event.kafka.componentmeta;
+
+import com.github.packageurl.PackageURL;
+
+public record ComponentProjection(String purlCoordinates, Boolean internal, PackageURL purl) {
+}

src/main/java/org/dependencytrack/event/kafka/componentmeta/Handler.java
@@ -1,0 +1,8 @@
+package org.dependencytrack.event.kafka.componentmeta;
+
+import com.github.packageurl.MalformedPackageURLException;
+import org.dependencytrack.model.IntegrityMetaComponent;
+
+public interface Handler {
+    IntegrityMetaComponent handle() throws MalformedPackageURLException;
+}

commit 02cdef2c57a42b049d38762941eddaf5e328c368

Fix NPE during BOM processing when component doesn't have a PURL
Seems to be a regression introduced in https://github.com/DependencyTrack/hyades-apiserver/pull/339

Signed-off-by: nscuro <nscuro@protonmail.com>

src/main/java/org/dependencytrack/tasks/BomUploadProcessingTask.java
@@ -321,14 +321,16 @@ public class BomUploadProcessingTask implements Subscriber {
                     // The constructors of ComponentRepositoryMetaAnalysisEvent and ComponentVulnerabilityAnalysisEvent
                     // merely call a few getters on it, but the component object itself is not passed around.
                     // Detaching would imply additional database interactions that we'd rather not do.
-                    boolean result = SUPPORTED_PACKAGE_URLS_FOR_INTEGRITY_CHECK.contains(component.getPurl().getType());
-                    ComponentRepositoryMetaAnalysisEvent event;
-                    if (result) {
-                        event = createRepoMetaAnalysisEvent(component, qm);
-                    } else {
-                        event = new ComponentRepositoryMetaAnalysisEvent(component.getUuid(), component.getPurlCoordinates().toString(), component.isInternal(), FetchMeta.FETCH_META_LATEST_VERSION);
+                    if (component.getPurl() != null) {
+                        boolean result = SUPPORTED_PACKAGE_URLS_FOR_INTEGRITY_CHECK.contains(component.getPurl().getType());
+                        ComponentRepositoryMetaAnalysisEvent event;
+                        if (result) {
+                            event = createRepoMetaAnalysisEvent(component, qm);
+                        } else {
+                            event = new ComponentRepositoryMetaAnalysisEvent(component.getUuid(), component.getPurlCoordinates().toString(), component.isInternal(), FetchMeta.FETCH_META_LATEST_VERSION);
+                        }
+                        repoMetaAnalysisEvents.add(event);
                     }
-                    repoMetaAnalysisEvents.add(event);
                     vulnAnalysisEvents.add(new ComponentVulnerabilityAnalysisEvent(
                             ctx.uploadToken, component, VulnerabilityAnalysisLevel.BOM_UPLOAD_ANALYSIS, component.isNew()));
                 }

src/test/java/org/dependencytrack/tasks/BomUploadProcessingTaskTest.java
@@ -54,6 +54,7 @@ import java.util.UUID;
 
 import static org.apache.commons.io.IOUtils.resourceToURL;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 import static org.dependencytrack.assertion.Assertions.assertConditionWithTimeout;
 import static org.dependencytrack.model.WorkflowStatus.CANCELLED;
 import static org.dependencytrack.model.WorkflowStatus.COMPLETED;
@@ -671,6 +672,28 @@ public class BomUploadProcessingTaskTest extends AbstractPostgresEnabledTest {
                     assertThat(notification.getGroup()).isEqualTo(Group.GROUP_BOM_PROCESSED);
                 }
         );
+    }
+
+    @Test
+    public void informWithComponentWithoutPurl() throws Exception {
+        Project project = qm.createProject("Acme Example", null, "1.0", null, null, null, true, false);
+
+        final var bomUploadEvent = new BomUploadEvent(qm.detach(Project.class, project.getId()), createTempBomFile("bom-no-purl.json"));
+        qm.createWorkflowSteps(bomUploadEvent.getChainIdentifier());
+        new BomUploadProcessingTask().inform(bomUploadEvent);
+
+        await("BOM processing")
+                .atMost(Duration.ofSeconds(5))
+                .untilAsserted(() -> assertThat(kafkaMockProducer.history()).satisfiesExactly(
+                        event -> assertThat(event.topic()).isEqualTo(KafkaTopics.NOTIFICATION_PROJECT_CREATED.name()),
+                        event -> assertThat(event.topic()).isEqualTo(KafkaTopics.NOTIFICATION_BOM.name()),
+                        event -> assertThat(event.topic()).isEqualTo(KafkaTopics.VULN_ANALYSIS_COMMAND.name()),
+                        // (No REPO_META_ANALYSIS_COMMAND event because the component doesn't have a PURL)
+                        event -> assertThat(event.topic()).isEqualTo(KafkaTopics.NOTIFICATION_BOM.name())
+                ));
+
+        assertThat(qm.getAllComponents(project))
+                .satisfiesExactly(component -> assertThat(component.getName()).isEqualTo("acme-lib"));
     }
 
     private static File createTempBomFile(final String testFileName) throws Exception {