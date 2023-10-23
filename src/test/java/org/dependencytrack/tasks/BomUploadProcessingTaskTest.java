edea7c2e6bb46b7846770a3445edfa18fdd6f903
vithikashukla <vithika.shukla@citi.com>
2023-10-23 13:33:42 +0100

    upgrade cyclonedx-core-java version
    Signed-off-by: vithikashukla <vithika.shukla@citi.com>

--- src/test/java/org/dependencytrack/tasks/BomUploadProcessingTaskTest.java
+++ src/test/java/org/dependencytrack/tasks/BomUploadProcessingTaskTest.java
@@ -102,7 +102,7 @@ public class BomUploadProcessingTaskTest extends AbstractPostgresEnabledTest {
         qm.getPersistenceManager().refresh(project);
         assertThat(project.getClassifier()).isEqualTo(Classifier.APPLICATION);
         assertThat(project.getLastBomImport()).isNotNull();
-        assertThat(project.getLastBomImportFormat()).isEqualTo("CycloneDX 1.4");
+        assertThat(project.getLastBomImportFormat()).isEqualTo("CycloneDX 1.5");
         assertThat(project.getExternalReferences()).isNotNull();
         assertThat(project.getExternalReferences()).hasSize(4);
 
@@ -187,7 +187,7 @@ public class BomUploadProcessingTaskTest extends AbstractPostgresEnabledTest {
         qm.getPersistenceManager().refresh(project);
         assertThat(project.getClassifier()).isEqualTo(Classifier.APPLICATION);
         assertThat(project.getLastBomImport()).isNotNull();
-        assertThat(project.getLastBomImportFormat()).isEqualTo("CycloneDX 1.4");
+        assertThat(project.getLastBomImportFormat()).isEqualTo("CycloneDX 1.5");
         assertThat(project.getExternalReferences()).isNotNull();
         assertThat(project.getExternalReferences()).hasSize(4);
 
