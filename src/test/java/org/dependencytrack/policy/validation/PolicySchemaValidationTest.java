270588612c040dcf08a8ea3448b862325823b8e2
Sahiba Mittal <sahibamittal98@gmail.com>
2023-12-05 11:58:31 +0000

    schema updated

--- src/test/java/org/dependencytrack/policy/validation/PolicySchemaValidationTest.java
+++ src/test/java/org/dependencytrack/policy/validation/PolicySchemaValidationTest.java
@@ -48,7 +48,7 @@ public class PolicySchemaValidationTest {
                     assertThat(error.getMessage()).isEqualTo("$.created: 2023-11-22T06:06Z is an invalid date-time");
                 },
                 error -> {
-                    assertThat(error.getMessage()).isEqualTo("$.analysis.justification: does not have a value in the enumeration [CODE_NOT_PRESENT, CODE_NOT_REACHABLE, REQUIRES_CONFIGURATION, REQUIRES_DEPENDENCY, REQUIRES_ENVIRONMENT, PROTECTED_BY_COMPILER, PROTECTED_AT_RUNTIME, PROTECTED_AT_PERIMETER, PROTECTED_BY_MITIGATING_CONTROL, NOT_SET]");
+                    assertThat(error.getMessage()).isEqualTo("$.analysis.justification: does not have a value in the enumeration [CODE_NOT_PRESENT, CODE_NOT_REACHABLE, REQUIRES_CONFIGURATION, REQUIRES_DEPENDENCY, REQUIRES_ENVIRONMENT, PROTECTED_BY_COMPILER, PROTECTED_AT_RUNTIME, PROTECTED_AT_PERIMETER, PROTECTED_BY_MITIGATING_CONTROL]");
                 },
                 error -> {
                     assertThat(error.getMessage()).isEqualTo("$.ratings[0].severity: does not have a value in the enumeration [CRITICAL, HIGH, MEDIUM, LOW, INFO, UNASSIGNED]");
