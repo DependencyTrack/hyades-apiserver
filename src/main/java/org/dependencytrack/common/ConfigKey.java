776cdc698218a83a89b921e66764bbf111e1a266
mehab <meha.bhargava@citi.com>
2023-12-13 16:00:40 +0000

    unit tests added
    Signed-off-by: mehab <meha.bhargava@citi.com>

--- src/main/java/org/dependencytrack/common/ConfigKey.java
+++ src/main/java/org/dependencytrack/common/ConfigKey.java
@@ -86,6 +86,7 @@ public enum ConfigKey implements Config.Key {
     VULNERABILITY_POLICY_S3_SECRET_KEY("vulnerability.policy.s3.secret.key", null),
     VULNERABILITY_POLICY_S3_BUCKET_NAME("vulnerability.policy.s3.bucket.name", null),
     VULNERABILITY_POLICY_S3_FILE_NAME("vulnerability.policy.s3.file.name", null),
+    VULNERABILITY_POLICY_S3_REGION("vulnerability.policy.s3.region", null),
     RUN_MIGRATIONS("run.migrations", true);
 
     private final String propertyName;
