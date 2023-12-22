59274ec937d176a1dd623e5be9129ac252bbce8b
Sahiba Mittal <sahibamittal98@gmail.com>
2023-12-22 11:57:49 +0000

    WIP

--- src/test/java/org/dependencytrack/persistence/jdbi/NotificationSubjectDaoTest.java
+++ src/test/java/org/dependencytrack/persistence/jdbi/NotificationSubjectDaoTest.java
@@ -14,6 +14,7 @@ import org.dependencytrack.model.VulnerabilityAnalysisLevel;
 import org.dependencytrack.persistence.CweImporter;
 import org.dependencytrack.proto.notification.v1.NewVulnerabilitySubject;
 import org.dependencytrack.proto.notification.v1.NewVulnerableDependencySubject;
+import org.dependencytrack.proto.notification.v1.VulnerabilityAnalysisDecisionChangeSubject;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -449,4 +450,186 @@ public class NotificationSubjectDaoTest extends AbstractPostgresEnabledTest {
                         """);
     }
 
+    @Test
+    public void testGetForProjectAuditChange() {
+        final var project = new Project();
+        project.setName("projectName");
+        project.setVersion("projectVersion");
+        project.setDescription("projectDescription");
+        project.setPurl("projectPurl");
+        qm.persist(project);
+        qm.bind(project, List.of(
+                qm.createTag("projectTagA"),
+                qm.createTag("projectTagB")
+        ));
+
+        final var component = new Component();
+        component.setProject(project);
+        component.setGroup("componentGroup");
+        component.setName("componentName");
+        component.setVersion("componentVersion");
+        component.setPurl("componentPurl");
+        component.setMd5("componentMd5");
+        component.setSha1("componentSha1");
+        component.setSha256("componentSha256");
+        component.setSha512("componentSha512");
+        qm.persist(component);
+
+        final var vulnA = new Vulnerability();
+        vulnA.setVulnId("CVE-100");
+        vulnA.setSource(Vulnerability.Source.NVD);
+        vulnA.setTitle("vulnATitle");
+        vulnA.setSubTitle("vulnASubTitle");
+        vulnA.setDescription("vulnADescription");
+        vulnA.setRecommendation("vulnARecommendation");
+        vulnA.setSeverity(Severity.MEDIUM);
+        vulnA.setCvssV2BaseScore(BigDecimal.valueOf(1.1));
+        vulnA.setCvssV3BaseScore(BigDecimal.valueOf(2.2));
+        vulnA.setOwaspRRBusinessImpactScore(BigDecimal.valueOf(3.3));
+        vulnA.setOwaspRRLikelihoodScore(BigDecimal.valueOf(4.4));
+        vulnA.setOwaspRRTechnicalImpactScore(BigDecimal.valueOf(5.5));
+        vulnA.setCwes(List.of(666, 777));
+        qm.persist(vulnA);
+
+        final var vulnB = new Vulnerability();
+        vulnB.setVulnId("CVE-200");
+        vulnB.setSource(Vulnerability.Source.NVD);
+        qm.persist(vulnB);
+
+        final var vulnAlias = new VulnerabilityAlias();
+        vulnAlias.setCveId("CVE-100");
+        vulnAlias.setGhsaId("GHSA-100");
+        qm.synchronizeVulnerabilityAlias(vulnAlias);
+
+        qm.addVulnerability(vulnA, component, AnalyzerIdentity.INTERNAL_ANALYZER);
+        qm.addVulnerability(vulnB, component, AnalyzerIdentity.INTERNAL_ANALYZER);
+
+        // Suppress vulnB, it should not appear in the query results.
+        qm.makeAnalysis(component, vulnB, AnalysisState.FALSE_POSITIVE, null, null, null, true);
+        qm.makeAnalysis(component, vulnA, AnalysisState.NOT_AFFECTED, null, null, null, false);
+
+        final List<VulnerabilityAnalysisDecisionChangeSubject> subjects = JdbiFactory.jdbi(qm).withExtension(NotificationSubjectDao.class,
+                dao -> dao.getForProjectAuditChange(component.getUuid(), List.of(vulnA.getId(), vulnB.getId())));
+
+        assertThat(subjects.size()).isEqualTo(2);
+        assertThat(subjects.get(0)).satisfies(subject ->
+                assertThatJson(JsonFormat.printer().print(subject))
+                        .withMatcher("projectUuid", equalTo(project.getUuid().toString()))
+                        .withMatcher("componentUuid", equalTo(component.getUuid().toString()))
+                        .withMatcher("vulnUuid", equalTo(vulnA.getUuid().toString()))
+                        .isEqualTo("""
+                                {
+                                     "component": {
+                                         "uuid": "${json-unit.matches:componentUuid}",
+                                         "group": "componentGroup",
+                                         "name": "componentName",
+                                         "version": "componentVersion",
+                                         "purl": "componentPurl",
+                                         "md5": "componentmd5",
+                                         "sha1": "componentsha1",
+                                         "sha256": "componentsha256",
+                                         "sha512": "componentsha512"
+                                     },
+                                     "project": {
+                                         "uuid": "${json-unit.matches:projectUuid}",
+                                         "name": "projectName",
+                                         "version": "projectVersion",
+                                         "description": "projectDescription",
+                                         "purl": "projectPurl",
+                                         "tags": [
+                                             "projecttaga",
+                                             "projecttagb"
+                                         ]
+                                     },
+                                     "vulnerability": {
+                                         "uuid": "${json-unit.matches:vulnUuid}",
+                                         "vulnId": "CVE-100",
+                                         "source": "NVD",
+                                         "aliases": [
+                                             {
+                                                 "vulnId": "GHSA-100",
+                                                 "source": "GITHUB"
+                                             }
+                                         ],
+                                         "title": "vulnATitle",
+                                         "subtitle": "vulnASubTitle",
+                                         "description": "vulnADescription",
+                                         "recommendation": "vulnARecommendation",
+                                         "cvssv2": 1.1,
+                                         "cvssv3": 2.2,
+                                         "owaspRRLikelihood": 4.4,
+                                         "owaspRRTechnicalImpact": 5.5,
+                                         "owaspRRBusinessImpact": 3.3,
+                                         "severity": "LOW",
+                                         "cwes": [
+                                             {
+                                                 "cweId": 666,
+                                                 "name": "Operation on Resource in Wrong Phase of Lifetime"
+                                             },
+                                             {
+                                                 "cweId": 777,
+                                                 "name": "Regular Expression without Anchors"
+                                             }
+                                         ]
+                                     },
+                                     "analysis": {
+                                         "component": {
+                                             "uuid": "${json-unit.matches:componentUuid}",
+                                             "group": "componentGroup",
+                                             "name": "componentName",
+                                             "version": "componentVersion",
+                                             "purl": "componentPurl",
+                                             "md5": "componentmd5",
+                                             "sha1": "componentsha1",
+                                             "sha256": "componentsha256",
+                                             "sha512": "componentsha512"
+                                         },
+                                         "project": {
+                                             "uuid": "${json-unit.matches:projectUuid}",
+                                             "name": "projectName",
+                                             "version": "projectVersion",
+                                             "description": "projectDescription",
+                                             "purl": "projectPurl",
+                                             "tags": [
+                                                 "projecttaga",
+                                                 "projecttagb"
+                                             ]
+                                         },
+                                         "vulnerability": {
+                                             "uuid": "${json-unit.matches:vulnUuid}",
+                                             "vulnId": "CVE-100",
+                                             "source": "NVD",
+                                             "aliases": [
+                                                 {
+                                                     "vulnId": "GHSA-100",
+                                                     "source": "GITHUB"
+                                                 }
+                                             ],
+                                             "title": "vulnATitle",
+                                             "subtitle": "vulnASubTitle",
+                                             "description": "vulnADescription",
+                                             "recommendation": "vulnARecommendation",
+                                             "cvssv2": 1.1,
+                                             "cvssv3": 2.2,
+                                             "owaspRRLikelihood": 4.4,
+                                             "owaspRRTechnicalImpact": 5.5,
+                                             "owaspRRBusinessImpact": 3.3,
+                                             "severity": "LOW",
+                                             "cwes": [
+                                                 {
+                                                     "cweId": 666,
+                                                     "name": "Operation on Resource in Wrong Phase of Lifetime"
+                                                 },
+                                                 {
+                                                     "cweId": 777,
+                                                     "name": "Regular Expression without Anchors"
+                                                 }
+                                             ]
+                                         },
+                                         "state": "NOT_AFFECTED",
+                                         "suppressed": false
+                                     }
+                                 }
+                                """));
+    }
 }
