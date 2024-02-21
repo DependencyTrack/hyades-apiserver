cba0e498b12a3b9408ca3d691408c6018f3aeed2
Sahiba Mittal <sahiba.mittal@citi.com>
2024-02-21 14:37:44 +0000

    Update Cwe.java

--- src/main/java/org/dependencytrack/model/Cwe.java
+++ src/main/java/org/dependencytrack/model/Cwe.java
@@ -32,7 +32,6 @@ import javax.jdo.annotations.PrimaryKey;
 import javax.jdo.annotations.Unique;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
