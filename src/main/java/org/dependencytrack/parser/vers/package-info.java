cbfb826dfc3d3d9d30d477a61322ff6b333e259c
Sahiba Mittal <sahibamittal98@gmail.com>
2024-03-22 17:28:42 +0000

    Replaced custom Vers with versatile library (#598)

--- src/main/java/org/dependencytrack/parser/vers/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Package vers contains a simplified version of the versatile library.
- * <p>
- * Most notably, this version is missing logic for version comparisons, and version range simplification.
- *
- * @see <a href="https://github.com/nscuro/versatile">versatile</a>
- */
-package org.dependencytrack.parser.vers;
