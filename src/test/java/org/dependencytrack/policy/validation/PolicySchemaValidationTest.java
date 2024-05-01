f90a73ecd59d09e2c242d89d417f4a65ac5a3d45
Sahiba Mittal <sahibamittal98@gmail.com>
2024-05-01 11:52:48 +0100

    fix schema test

--- src/test/java/org/dependencytrack/policy/validation/PolicySchemaValidationTest.java
+++ src/test/java/org/dependencytrack/policy/validation/PolicySchemaValidationTest.java
@@ -62,7 +62,8 @@ public class PolicySchemaValidationTest {
                 error -> assertThat(error.getMessage()).isEqualTo("$.analysis.justification: does not have a value in the enumeration [CODE_NOT_PRESENT, CODE_NOT_REACHABLE, REQUIRES_CONFIGURATION, REQUIRES_DEPENDENCY, REQUIRES_ENVIRONMENT, PROTECTED_BY_COMPILER, PROTECTED_AT_RUNTIME, PROTECTED_AT_PERIMETER, PROTECTED_BY_MITIGATING_CONTROL]"),
                 error -> assertThat(error.getMessage()).isEqualTo("$.ratings[0].severity: does not have a value in the enumeration [CRITICAL, HIGH, MEDIUM, LOW, INFO, UNASSIGNED]"),
                 error -> assertThat(error.getMessage()).contains("$.ratings[0].vector: does not match the regex pattern"),
-                error -> assertThat(error.getMessage()).isEqualTo("$.ratings[0].score: string found, number expected")
+                error -> assertThat(error.getMessage()).isEqualTo("$.ratings[0].score: string found, number expected"),
+                error -> assertThat(error.getMessage()).isEqualTo("$.operationMode: is missing but it is required")
         );
     }
 }
