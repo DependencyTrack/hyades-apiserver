b822d6934930ab711560dadff9b0921d0f0fabbf
nscuro <nscuro@protonmail.com>
2024-06-16 17:27:03 +0200

    Remove dependency on `kafka-junit`
    The minimal value we get out of it doesn't justify pulling in Kafka in its entirety as a dependency, even if it's just in `test` scope.
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/test/java/org/dependencytrack/event/kafka/processor/api/ProcessorManagerTest.java
+++ src/test/java/org/dependencytrack/event/kafka/processor/api/ProcessorManagerTest.java
@@ -19,15 +19,17 @@
 package org.dependencytrack.event.kafka.processor.api;
 
 import alpine.Config;
-import net.mguenther.kafka.junit.ExternalKafkaCluster;
-import net.mguenther.kafka.junit.KeyValue;
-import net.mguenther.kafka.junit.SendKeyValues;
-import net.mguenther.kafka.junit.TopicConfig;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.dependencytrack.common.ConfigKey;
 import org.dependencytrack.event.kafka.KafkaTopics.Topic;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -44,6 +46,7 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -60,22 +63,38 @@ public class ProcessorManagerTest {
     public RedpandaContainer kafkaContainer = new RedpandaContainer(DockerImageName
             .parse("docker.redpanda.com/vectorized/redpanda:v24.1.7"));
 
-    private ExternalKafkaCluster kafka;
+    private AdminClient adminClient;
+    private Producer<String, String> producer;
     private Config configMock;
 
     @Before
     public void setUp() {
-        kafka = ExternalKafkaCluster.at(kafkaContainer.getBootstrapServers());
+        adminClient = AdminClient.create(Map.of(BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers()));
+        producer = new KafkaProducer<>(Map.ofEntries(
+                Map.entry(BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers()),
+                Map.entry(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class),
+                Map.entry(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
+        ));
 
         configMock = mock(Config.class);
         when(configMock.getProperty(eq(ConfigKey.KAFKA_BOOTSTRAP_SERVERS)))
                 .thenReturn(kafkaContainer.getBootstrapServers());
     }
 
+    @After
+    public void tearDown() {
+        if (adminClient != null) {
+            adminClient.close();
+        }
+        if (producer != null) {
+            producer.close();
+        }
+    }
+
     @Test
     public void testSingleRecordProcessor() throws Exception {
         final var inputTopic = new Topic<>("input", Serdes.String(), Serdes.String());
-        kafka.createTopic(TopicConfig.withName(inputTopic.name()).withNumberOfPartitions(3));
+        adminClient.createTopics(List.of(new NewTopic(inputTopic.name(), 3, (short) 1))).all().get();
 
         final var recordsProcessed = new AtomicInteger(0);
 
@@ -93,9 +112,7 @@ public class ProcessorManagerTest {
             processorManager.registerProcessor("foo", inputTopic, processor);
 
             for (int i = 0; i < 100; i++) {
-                kafka.send(SendKeyValues.to("input", List.of(new KeyValue<>("foo" + i, "bar" + i)))
-                        .with(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName())
-                        .with(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()));
+                producer.send(new ProducerRecord<>("input", "foo" + i, "bar" + i));
             }
 
             processorManager.startAll();
@@ -109,7 +126,7 @@ public class ProcessorManagerTest {
     @Test
     public void testSingleRecordProcessorRetry() throws Exception {
         final var inputTopic = new Topic<>("input", Serdes.String(), Serdes.String());
-        kafka.createTopic(TopicConfig.withName(inputTopic.name()).withNumberOfPartitions(3));
+        adminClient.createTopics(List.of(new NewTopic(inputTopic.name(), 3, (short) 1))).all().get();
 
         final var attemptsCounter = new AtomicInteger(0);
 
@@ -139,9 +156,7 @@ public class ProcessorManagerTest {
         try (final var processorManager = new ProcessorManager(UUID.randomUUID(), configMock)) {
             processorManager.registerProcessor("foo", inputTopic, processor);
 
-            kafka.send(SendKeyValues.to("input", List.of(new KeyValue<>("foo", "bar")))
-                    .with(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName())
-                    .with(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()));
+            producer.send(new ProducerRecord<>("input", "foo", "bar"));
 
             processorManager.startAll();
 
@@ -154,7 +169,7 @@ public class ProcessorManagerTest {
     @Test
     public void testBatchProcessor() throws Exception {
         final var inputTopic = new Topic<>("input", Serdes.String(), Serdes.String());
-        kafka.createTopic(TopicConfig.withName(inputTopic.name()).withNumberOfPartitions(3));
+        adminClient.createTopics(List.of(new NewTopic(inputTopic.name(), 3, (short) 1))).all().get();
 
         final var recordsProcessed = new AtomicInteger(0);
         final var actualBatchSizes = new ConcurrentLinkedQueue<>();
@@ -178,9 +193,7 @@ public class ProcessorManagerTest {
             processorManager.registerBatchProcessor("foo", inputTopic, recordProcessor);
 
             for (int i = 0; i < 1_000; i++) {
-                kafka.send(SendKeyValues.to("input", List.of(new KeyValue<>("foo" + i, "bar" + i)))
-                        .with(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName())
-                        .with(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()));
+                producer.send(new ProducerRecord<>("input", "foo" + i, "bar" + i));
             }
 
             processorManager.startAll();
@@ -196,8 +209,7 @@ public class ProcessorManagerTest {
     @Test
     public void testWithMaxConcurrencyMatchingPartitionCount() throws Exception {
         final var inputTopic = new Topic<>("input", Serdes.String(), Serdes.String());
-
-        kafka.createTopic(TopicConfig.withName(inputTopic.name()).withNumberOfPartitions(12));
+        adminClient.createTopics(List.of(new NewTopic(inputTopic.name(), 12, (short) 1))).all().get();
 
         when(configMock.getPassThroughProperties(eq("kafka.processor.foo")))
                 .thenReturn(Map.of(
@@ -218,9 +230,7 @@ public class ProcessorManagerTest {
             processorManager.registerProcessor("foo", inputTopic, processor);
 
             for (int i = 0; i < 100; i++) {
-                kafka.send(SendKeyValues.to("input", List.of(new KeyValue<>("foo" + i, "bar" + i)))
-                        .with(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName())
-                        .with(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()));
+                producer.send(new ProducerRecord<>("input", "foo" + i, "bar" + i));
             }
 
             processorManager.startAll();
@@ -234,11 +244,10 @@ public class ProcessorManagerTest {
     }
 
     @Test
-    public void testStartAllWithMissingTopics() {
+    public void testStartAllWithMissingTopics() throws Exception {
         final var inputTopicA = new Topic<>("input-a", Serdes.String(), Serdes.String());
         final var inputTopicB = new Topic<>("input-b", Serdes.String(), Serdes.String());
-
-        kafka.createTopic(TopicConfig.withName(inputTopicA.name()).withNumberOfPartitions(3));
+        adminClient.createTopics(List.of(new NewTopic(inputTopicA.name(), 3, (short) 1))).all().get();
 
         final Processor<String, String> processor = record -> {
         };
@@ -257,9 +266,9 @@ public class ProcessorManagerTest {
     }
 
     @Test
-    public void testProbeHealth() {
+    public void testProbeHealth() throws Exception {
         final var inputTopic = new Topic<>("input", Serdes.String(), Serdes.String());
-        kafka.createTopic(TopicConfig.withName(inputTopic.name()).withNumberOfPartitions(3));
+        adminClient.createTopics(List.of(new NewTopic(inputTopic.name(), 3, (short) 1))).all().get();
 
         final Processor<String, String> processor = record -> {
         };
