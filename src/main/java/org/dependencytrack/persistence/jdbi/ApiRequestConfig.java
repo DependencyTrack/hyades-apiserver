ee09fc94b3f191d71eadd6ffc26fa7201b165c4e
nscuro <nscuro@protonmail.com>
2024-06-26 18:09:28 +0200

    Add endpoint to list projects concisely
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/main/java/org/dependencytrack/persistence/jdbi/ApiRequestConfig.java
+++ src/main/java/org/dependencytrack/persistence/jdbi/ApiRequestConfig.java
@@ -34,7 +34,7 @@ public class ApiRequestConfig implements JdbiConfig<ApiRequestConfig> {
 
     // TODO: Make this configurable via annotation when needed (similar to @AllowOrdering).
     //   In some queries the PROJECT table may be aliased (e.g. as P).
-    private String projectAclProjectTableName = "PROJECT";
+    private String projectTableAlias = "PROJECT";
 
     @SuppressWarnings("unused")
     public ApiRequestConfig() {
@@ -44,7 +44,7 @@ public class ApiRequestConfig implements JdbiConfig<ApiRequestConfig> {
     private ApiRequestConfig(final ApiRequestConfig that) {
         this.orderingAllowedColumns = Set.copyOf(that.orderingAllowedColumns);
         this.orderingAlwaysBy = that.orderingAlwaysBy;
-        this.projectAclProjectTableName = that.projectAclProjectTableName;
+        this.projectTableAlias = that.projectTableAlias;
     }
 
     @Override
@@ -75,7 +75,7 @@ public class ApiRequestConfig implements JdbiConfig<ApiRequestConfig> {
     }
 
     String projectAclProjectTableName() {
-        return projectAclProjectTableName;
+        return projectTableAlias;
     }
 
     public record OrderingColumn(String name, String queryName) {
