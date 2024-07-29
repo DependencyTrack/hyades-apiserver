d4e64755f114ceba6ad62d648d8b49f75abdb341
Zachary Prebosnyak <91638307+zprebosnyak-lm@users.noreply.github.com>
2024-07-29 11:39:34 -0600

    fix: update tests
    Signed-off-by: Zachary Prebosnyak <91638307+zprebosnyak-lm@users.noreply.github.com>

--- src/test/java/org/dependencytrack/resources/v1/PermissionResourceTest.java
+++ src/test/java/org/dependencytrack/resources/v1/PermissionResourceTest.java
@@ -64,7 +64,7 @@ public class PermissionResourceTest extends ResourceTest {
         Assert.assertNull(response.getHeaderString(TOTAL_COUNT_HEADER));
         JsonArray json = parseJsonArray(response);
         Assert.assertNotNull(json);
-        Assert.assertEquals(12, json.size());
+        Assert.assertEquals(35, json.size());
         Assert.assertEquals("ACCESS_MANAGEMENT", json.getJsonObject(0).getString("name"));
         Assert.assertEquals("Allows the management of users, teams, and API keys", json.getJsonObject(0).getString("description"));
     }
