a699c8097c5686e8399b13c9d66709941fa64ff3
nscuro <nscuro@protonmail.com>
2024-09-19 17:35:57 +0200

    Enable `reWriteBatchedInserts` Postgres JDBC driver option in tests
    As per JDBC driver documentation (https://jdbc.postgresql.org/documentation/use/):
    
    This will change batch inserts from insert into foo (col1, col2, col3) values (1, 2, 3) into insert into foo (col1, col2, col3) values (1, 2, 3), (4, 5, 6) this provides 2-3x performance improvement
    
    Indeed, per default even batched JDBC statements are executed one-by-one, somewhat defeating the purpose of batching in the first place. This option allows us to get the true benefit out of performing batch operations.
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/main/java/org/dependencytrack/dev/DevServicesInitializer.java
+++ src/main/java/org/dependencytrack/dev/DevServicesInitializer.java
@@ -20,12 +20,12 @@ package org.dependencytrack.dev;
 
 import alpine.Config;
 import alpine.common.logging.Logger;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.dependencytrack.event.kafka.KafkaTopics;
 
-import jakarta.servlet.ServletContextEvent;
-import jakarta.servlet.ServletContextListener;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -92,6 +92,7 @@ public class DevServicesInitializer implements ServletContextListener {
             final Class<?> postgresContainerClass = Class.forName("org.testcontainers.containers.PostgreSQLContainer");
             final Constructor<?> postgresContainerConstructor = postgresContainerClass.getDeclaredConstructor(String.class);
             postgresContainer = (AutoCloseable) postgresContainerConstructor.newInstance(getProperty(DEV_SERVICES_IMAGE_POSTGRES));
+            postgresContainerClass.getMethod("withUrlParam", String.class, String.class).invoke(postgresContainer, "reWriteBatchedInserts", "true");
 
             // TODO: Detect when Apache Kafka is requested vs. when Kafka is requested,
             //   and pick the corresponding Testcontainers class accordingly.
