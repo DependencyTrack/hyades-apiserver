5506eb3ec5f0ecc6151fcb9f086b27177b10f49e
Jonathan Howard <jonathan.w.howard@lmco.com>
2025-02-12 08:57:59 -0600

    refactor: remove token class member from sync task
    Signed-off-by: Jonathan Howard <jonathan.w.howard@lmco.com>

--- apiserver/src/main/java/org/dependencytrack/tasks/GitLabSyncTask.java
+++ apiserver/src/main/java/org/dependencytrack/tasks/GitLabSyncTask.java
@@ -32,16 +32,14 @@ public class GitLabSyncTask implements LoggableSubscriber {
 
     private static final Logger LOGGER = Logger.getLogger(GitLabSyncTask.class);
     private final boolean isEnabled;
-    private final String accessToken;
 
-    public GitLabSyncTask(final String accessToken) {
+    public GitLabSyncTask() {
         final String groupName = GITLAB_ENABLED.getGroupName();
 
         try (final QueryManager qm = new QueryManager()) {
             final ConfigProperty enabled = qm.getConfigProperty(groupName, GITLAB_ENABLED.getPropertyName());
 
             this.isEnabled = enabled != null && Boolean.parseBoolean(enabled.getPropertyValue());
-            this.accessToken = accessToken;
         }
     }
 
@@ -54,12 +52,17 @@ public class GitLabSyncTask implements LoggableSubscriber {
             return;
         }
 
-        if (this.accessToken == null) {
+        GitLabSyncEvent gitLabSyncEvent = (GitLabSyncEvent) event;
+        String accessToken = gitLabSyncEvent.getAccessToken();
+
+        if (accessToken == null || accessToken.isEmpty()) {
             LOGGER.warn("GitLab syncing is enabled, but no access token was provided. Skipping.");
             return;
         }
 
         LOGGER.info("Starting GitLab sync task");
+
+        GitLabSyncer syncer = new GitLabSyncer(accessToken);
 
         // TODO: Get user GitLab project memberships (use alpine.security.crypto.DataEncryption for request)
         // TODO: Create Dependency-Track hierarchical project structure for user's GitLab projects
