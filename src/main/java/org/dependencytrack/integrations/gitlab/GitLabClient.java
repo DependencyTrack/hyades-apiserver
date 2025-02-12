e5e65a1b3b1061fbd4f214fcbcd408ed7bf4e68d
Ephraim Mensah <ephraim.e.mensah@lmco.com>
2025-02-12 08:57:59 -0600

    clean up remove unused import
    Signed-off-by: Ephraim Mensah <ephraim.e.mensah@lmco.com>

--- src/main/java/org/dependencytrack/integrations/gitlab/GitLabClient.java
+++ src/main/java/org/dependencytrack/integrations/gitlab/GitLabClient.java
@@ -22,10 +22,8 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.dependencytrack.auth.Permissions;
 import org.json.JSONArray;
