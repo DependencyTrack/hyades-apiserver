38060bc6f12636b58994ff01c5eb0cfd5ba3af06
nscuro <nscuro@protonmail.com>
2025-02-21 16:28:42 +0100

    Restrict portfolio metrics to only include projects a user has access to
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/main/java/org/dependencytrack/persistence/jdbi/MetricsDao.java
+++ src/main/java/org/dependencytrack/persistence/jdbi/MetricsDao.java
@@ -18,15 +18,87 @@
  */
 package org.dependencytrack.persistence.jdbi;
 
+import org.jdbi.v3.core.statement.Query;
+import org.jdbi.v3.sqlobject.SqlObject;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @since 5.6.0
  */
-public interface MetricsDao {
+public interface MetricsDao extends SqlObject {
+
+    // TODO:
+    //   * Get latest PROJECTMETRICS record for each project for each day.
+    //   * Calculate metrics sums across all projects for each day.
+    //   * Find a way to fill gaps (i.e. when no metrics exist for a project for a specific day)?
+    //   * Cache result for a few min in an unlogged table?
+    default List<Map<String, Object>> getPortfolioMetricsSince(@Bind Instant since) {
+        final Query query = getHandle().createQuery("""
+                with recursive
+                directly_accessible_project as(
+                  select "PROJECT_ID" as id
+                    from "PROJECT_ACCESS_TEAMS"
+                   inner join "PROJECT"
+                      on "PROJECT"."ID" = "PROJECT_ACCESS_TEAMS"."PROJECT_ID"
+                   where "TEAM_ID" = any(:projectAclTeamIds)
+                     and "PROJECT"."INACTIVE_SINCE" IS NULL
+                ),
+                accessible_project_child(id) as(
+                  select "ID" as id
+                    from "PROJECT"
+                   where "PARENT_PROJECT_ID" IN (select id from directly_accessible_project)
+                     and "PROJECT"."INACTIVE_SINCE" IS NULL
+                   union all
+                  select "PROJECT"."ID" as id
+                    from "PROJECT"
+                   inner join accessible_project_child
+                      on accessible_project_child.id = "PROJECT"."PARENT_PROJECT_ID"
+                     and "PROJECT"."INACTIVE_SINCE" IS NULL
+                ),
+                accessible_project as(
+                  select id
+                    from directly_accessible_project
+                   union all
+                  select id
+                    from accessible_project_child
+                ),
+                latest as(
+                  select distinct on (cast("LAST_OCCURRENCE" as date), "PROJECT_ID")
+                         "ID" as id
+                       , cast("LAST_OCCURRENCE" as date) as occurrence_date
+                       , "PROJECT_ID" as project_id
+                    from "PROJECTMETRICS"
+                   where "PROJECT_ID" in (select id from accessible_project)
+                     and "LAST_OCCURRENCE" >= :since
+                ),
+                dates as(
+                  select cast(t.the_day as date) as day
+                    from generate_series(:since, now(), '1 day') as t(the_day)
+                )
+                select dates.day as day
+                     , to_char(dates.day, 'yyyy-mm-dd') as day_str
+                     , sum("PROJECTMETRICS"."CRITICAL") as critical
+                     , sum("PROJECTMETRICS"."HIGH") as high
+                     , sum("PROJECTMETRICS"."MEDIUM") as medium
+                  from dates
+                  left join latest
+                    on latest.occurrence_date = dates.day
+                  left join "PROJECTMETRICS"
+                    on "PROJECTMETRICS"."ID" = latest.id
+                group by day
+                """);
+
+        return query
+                .bind("since", since)
+                .mapToMap()
+                .list();
+    }
 
     @SqlUpdate("""
             DELETE
