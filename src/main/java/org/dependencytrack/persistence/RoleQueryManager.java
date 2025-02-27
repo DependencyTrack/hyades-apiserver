0bcefd8983916fb74848e92d9b48e3f921d346ce
jmayer-lm <johnny.w.mayer.iii@lmco.com>
2025-02-27 12:16:22 -0500

    Add /v1/user Endpoints & CRUD Method Stubs (#7)
    * Add POST and DELETE role endpoints to UserResource
    
    Signed-off-by: Johnny Mayer <johnny.w.mayer.iii@lmco.com>
    
    * apply code style suggestions
    
    Signed-off-by: Johnny Mayer <johnny.w.mayer.iii@lmco.com>
    
    * Add CRUD method stubs to RoleQueryManager and QueryManager
    
    Signed-off-by: Johnny Mayer <johnny.w.mayer.iii@lmco.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: jhoward-lm <140011346+jhoward-lm@users.noreply.github.com>
    Signed-off-by: Allen Shearin <allen.p.shearin@gmail.com>
    
    ---------
    
    Signed-off-by: Johnny Mayer <johnny.w.mayer.iii@lmco.com>
    Signed-off-by: Allen Shearin <allen.p.shearin@gmail.com>
    Co-authored-by: Allen Shearin <allen.p.shearin@gmail.com>
    Co-authored-by: jhoward-lm <140011346+jhoward-lm@users.noreply.github.com>
    Signed-off-by: Allen Shearin <allen.p.shearin@gmail.com>

--- src/main/java/org/dependencytrack/persistence/RoleQueryManager.java
+++ src/main/java/org/dependencytrack/persistence/RoleQueryManager.java
@@ -1,17 +1,14 @@
 package org.dependencytrack.persistence;
 
 import java.nio.file.attribute.UserPrincipal;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import javax.jdo.PersistenceManager;
-import javax.jdo.Query;
 
 import org.dependencytrack.model.Role;
 
 import alpine.common.logging.Logger;
-import alpine.model.Permission;
 import alpine.resources.AlpineRequest;
 
 final class RoleQueryManager extends QueryManager implements IQueryManager {
@@ -24,6 +21,31 @@ final class RoleQueryManager extends QueryManager implements IQueryManager {
 
     RoleQueryManager(final PersistenceManager pm, final AlpineRequest request) {
         super(pm, request);
+    }
+
+    public Role createRole(Role role) {
+        // TODO:Implement role creation logic
+        return role;
+    }
+
+    public List<Role> getRoles() {
+        // TODO:Implement role retrieval logic
+        return Collections.emptyList();
+    }
+
+    public Role getRole(String uuid) {
+        // TODO:Implement role retrieval logic
+        return null;
+    }
+
+    public Role updateRole(Role role) {
+        // TODO:Implement role update logic
+        return role;
+    }
+
+    public boolean deleteRole(String uuid, boolean value) {
+        // TODO:Implement role deletion logic
+        return false;
     }
 
     boolean addRoleToUser(UserPrincipal principal, Role role, String roleName, String projectName){
