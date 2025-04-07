30819d7b888bc54836bb0819ddeecb69004e3042
nscuro <nscuro@protonmail.com>
2025-04-07 15:20:18 +0200

    Convert to multi-module Maven project
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- dev/scripts/update-spdx-license-list.sh
+++ dev/scripts/update-spdx-license-list.sh
@@ -20,7 +20,7 @@
 set -euxo pipefail
 
 SCRIPT_DIR="$(cd -P -- "$(dirname "$0")" && pwd -P)"
-LICENSE_LIST_DATA_DIR="$(cd -P -- "${SCRIPT_DIR}/../../src/main/resources/license-list-data" && pwd -P)"
+LICENSE_LIST_DATA_DIR="$(cd -P -- "${SCRIPT_DIR}/../../apiserver/src/main/resources/license-list-data" && pwd -P)"
 TMP_DOWNLOAD_FILE="$(mktemp)"
 
 gh -R spdx/license-list-data release download "v$1" \
