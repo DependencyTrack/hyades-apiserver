cc62b45955a691d14500f47c1f919166147cc464
nscuro <nscuro@protonmail.com>
2025-04-30 01:08:02 +0200

    Remove unused Alpine code
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- alpine/alpine-infra/src/main/java/alpine/persistence/JdoProperties.java
+++ alpine/alpine-infra/src/main/java/alpine/persistence/JdoProperties.java
@@ -20,7 +20,7 @@ package alpine.persistence;
 
 import alpine.Config;
 import org.datanucleus.PropertyNames;
-import org.datanucleus.store.rdbms.RDBMSPropertyNames;
+
 import java.util.Properties;
 
 /**
@@ -31,36 +31,6 @@ import java.util.Properties;
 public final class JdoProperties {
 
     private JdoProperties() { }
-
-    /**
-     * @return The pre-populated {@link Properties} for {@link javax.jdo.PersistenceManagerFactory} creation
-     * @deprecated Assemble DataNucleus properties in context of {@link javax.jdo.PersistenceManagerFactory} creation instead
-     */
-    @Deprecated(forRemoval = true)
-    public static Properties get() {
-        final Properties properties = new Properties();
-        properties.put("javax.jdo.option.ConnectionURL", Config.getInstance().getProperty(Config.AlpineKey.DATABASE_URL));
-        properties.put("javax.jdo.option.ConnectionDriverName", Config.getInstance().getProperty(Config.AlpineKey.DATABASE_DRIVER));
-        properties.put("javax.jdo.option.ConnectionUserName", Config.getInstance().getProperty(Config.AlpineKey.DATABASE_USERNAME));
-        properties.put("javax.jdo.option.ConnectionPassword", Config.getInstance().getPropertyOrFile(Config.AlpineKey.DATABASE_PASSWORD));
-        if (Config.getInstance().getPropertyAsBoolean(Config.AlpineKey.DATABASE_POOL_ENABLED)) {
-            properties.put(PropertyNames.PROPERTY_CONNECTION_POOLINGTYPE, "HikariCP");
-            properties.put(RDBMSPropertyNames.PROPERTY_CONNECTION_POOL_MAX_POOL_SIZE, Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_MAX_SIZE));
-            properties.put(RDBMSPropertyNames.PROPERTY_CONNECTION_POOL_IDLE_TIMEOUT, Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_IDLE_TIMEOUT));
-            properties.put(RDBMSPropertyNames.PROPERTY_CONNECTION_POOL_MAX_LIFETIME, Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_MAX_LIFETIME));
-            properties.put(RDBMSPropertyNames.PROPERTY_CONNECTION_POOL_MIN_IDLE, Config.getInstance().getProperty(Config.AlpineKey.DATABASE_POOL_MIN_IDLE));
-        }
-        properties.put(PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_DATABASE, "true");
-        properties.put(PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_TABLES, "true");
-        properties.put(PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_COLUMNS, "true");
-        properties.put(PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_CONSTRAINTS, "true");
-        properties.put(PropertyNames.PROPERTY_SCHEMA_GENERATE_DATABASE_MODE, "create");
-        properties.put(PropertyNames.PROPERTY_QUERY_JDOQL_ALLOWALL, "true");
-        if (Config.getInstance().getPropertyAsBoolean(Config.AlpineKey.METRICS_ENABLED)) {
-            properties.put(PropertyNames.PROPERTY_ENABLE_STATISTICS, "true");
-        }
-        return properties;
-    }
 
     public static Properties unit() {
         final Properties properties = new Properties();
@@ -76,6 +46,7 @@ public final class JdoProperties {
         properties.put(PropertyNames.PROPERTY_SCHEMA_AUTOCREATE_CONSTRAINTS, "true");
         properties.put(PropertyNames.PROPERTY_SCHEMA_GENERATE_DATABASE_MODE, "create");
         properties.put(PropertyNames.PROPERTY_QUERY_JDOQL_ALLOWALL, "true");
+        properties.put(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
         properties.put(PropertyNames.PROPERTY_RETAIN_VALUES, "true");
         properties.putAll(Config.getInstance().getPassThroughProperties("datanucleus"));
         return properties;
