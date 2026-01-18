0645cc155635c4c1cab586ec6120b7f0b907446b
nscuro <nscuro@protonmail.com>
2026-01-18 19:30:28 +0100

    Apply consistent module naming
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- alpine/alpine-model/src/test/java/alpine/model/AboutTest.java
+++ alpine/alpine-model/src/test/java/alpine/model/AboutTest.java
@@ -35,7 +35,7 @@ public class AboutTest {
         Assertions.assertEquals("1970-01-01 00:00:00", about.getTimestamp());
         Assertions.assertEquals("00000000-0000-0000-0000-000000000000", about.getUuid());
 
-        Assertions.assertEquals("alpine-model", about.getFramework().getName());
+        Assertions.assertEquals("Alpine :: Model", about.getFramework().getName());
         //Assertions.assertTrue(about.getFramework().getVersion().startsWith("3."));
         Assertions.assertTrue(about.getFramework().getTimestamp().startsWith("20"));
         Assertions.assertTrue(UuidUtil.isValidUUID(about.getFramework().getUuid()));
